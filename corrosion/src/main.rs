d668936dfa04884a06af0e222e34c1a407ecdfcb
Tom <tomoliverball@gmail.com>
2019-01-31 21:41:15 +0000

    Started the real corrosion project...

---
+++ corrosion/src/main.rs
@@ -0,0 +1,7 @@
+mod square;
+
+fn main() {
+    println!("Hello, world!");
+    let x = square::H1;
+    println!("{}", x.index);
+}
---
+++ corrosion/src/square.rs
@@ -0,0 +1,12 @@
+#[derive(Debug)]
+pub struct Square {
+    pub index: u8,
+}
+
+
+pub const H1: Square = Square { index: 0 };
+
+impl Square {
+
+}
+    
