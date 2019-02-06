d1b4a6080a866d30be08d4178f9f9a2eca3b87b2
Tom <tomoliverball@gmail.com>
2019-02-06 22:57:33 +0000

    Starting to understand things a bit more

--- corrosion/src/main.rs
+++ corrosion/src/main.rs
@@ -4,16 +4,23 @@ mod dir;
 use self::square::Square;
 
 fn main() {
-    let sq: Square = Square {i: 0, rank: 0, file: 0, loc: 0 };
-    let sq2 = square::H1;
-    let sq3 = square::H1;
-    let ref sqref = square::H1;
-    first2(sq2);
-//    first(sq);
-    second(sqref);
-    second(sqref);
-    //third(&mut sq);
-    println!("{:?}", sq3);
+    //let mut sq: Square = Square {i: 0, rank: 0, file: 0, loc: 0 };
+    let ref mut sqref2 = Square {i: 0, rank: 0, file: 0, loc: 0 };
+    some_func(sqref2);
+    println!("{:?}", sqref2);
+//    let sq2 = square::H1;
+//    let sq3 = square::H1;
+//    let ref sqref = square::H1;
+//    first2(sq2);
+////    first(sq);
+//    second(sqref);
+//    second(sqref);
+//    //third(&mut sq);
+//    println!("{:?}", sq3);
+}
+
+fn some_func(mut input_ref: &Square) {
+    input_ref = &square::H1;
 }
 
 fn first2(square: Square) {
