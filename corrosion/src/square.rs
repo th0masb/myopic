d26f25293466112a3c69a1e4509d4831c98e1730
Tom <tomoliverball@gmail.com>
2019-02-10 11:54:57 +0000

    Going to make Square and dir into Copyable value types

--- corrosion/src/square.rs
+++ corrosion/src/square.rs
@@ -6,6 +6,8 @@ pub struct Square {
     pub i: i8,
     pub rank: i8,
     pub file: i8,
+
+    // Get rid of these and make copyable type
     pub loc: u64,
     pub name: &'static str
 }
@@ -129,14 +131,14 @@ pub const C8: Square = Square::init(61, "C8");
 pub const B8: Square = Square::init(62, "B8");
 pub const A8: Square = Square::init(63, "A8");
 
-pub const ALL: [Square; 64] = [
-    H1, G1, F1, E1, D1, C1, B1, A1,
-    H2, G2, F2, E2, D2, C2, B2, A2,
-    H3, G3, F3, E3, D3, C3, B3, A3,
-    H4, G4, F4, E4, D4, C4, B4, A4,
-    H5, G5, F5, E5, D5, C5, B5, A5,
-    H6, G6, F6, E6, D6, C6, B6, A6,
-    H7, G7, F7, E7, D7, C7, B7, A7,
-    H8, G8, F8, E8, D8, C8, B8, A8
+pub const ALL: [&'static Square; 64] = [
+    &H1, &G1, &F1, &E1, &D1, &C1, &B1, &A1,
+    &H2, &G2, &F2, &E2, &D2, &C2, &B2, &A2,
+    &H3, &G3, &F3, &E3, &D3, &C3, &B3, &A3,
+    &H4, &G4, &F4, &E4, &D4, &C4, &B4, &A4,
+    &H5, &G5, &F5, &E5, &D5, &C5, &B5, &A5,
+    &H6, &G6, &F6, &E6, &D6, &C6, &B6, &A6,
+    &H7, &G7, &F7, &E7, &D7, &C7, &B7, &A7,
+    &H8, &G8, &F8, &E8, &D8, &C8, &B8, &A8
 ];
 
--- corrosion/src/squareset.rs
+++ corrosion/src/squareset.rs
@@ -1,3 +1,15 @@
 use super::square::Square;
+use super::square;
 
 pub struct SquareSet(u64);
+
+struct SquareSetIterator { src: u64, counter: usize }
+
+impl Iterator for SquareSetIterator {
+    type Item = &'static Square;
+
+    fn next(&mut self) -> Option<&'static Square> {
+        let x = square::ALL[1].loc;
+        Some(square::ALL[0])
+    }
+}
