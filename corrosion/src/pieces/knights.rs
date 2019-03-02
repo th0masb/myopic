959f06967cbe1f18b311bfae57278095fba722b4
Tom <tomoliverball@gmail.com>
2019-03-02 13:28:02 +0000

    added tests for knights

--- corrosion/src/pieces/knights.rs
+++ corrosion/src/pieces/knights.rs
@@ -39,6 +39,19 @@ mod white_test {
     fn test_control() {
         let (wn, zero) = (WhiteKnight, BitBoard::EMPTY);
         assert_eq!(D1 | C2 | C4 | D5 | F5 | G4 | G2 | F1, wn.controlset(E3, zero, zero));
+        assert_eq!(A4 | C4 | D3 | D1, wn.controlset(B2, zero, zero));
+    }
+
+    #[test]
+    fn test_moveset() {
+        let wn = WhiteKnight;
+        assert_eq!(A4 | C4 | D3, wn.moveset(B2, D1 | B1, A4 | D7));
+    }
+
+    #[test]
+    fn test_attackset() {
+        let wn = WhiteKnight;
+        assert_eq!(A4.as_set(), wn.attackset(B2, D1 | B1, A4 | D7));
     }
 }
 
@@ -55,3 +68,29 @@ impl Piece for BlackKnight {
         CONTROL[location.i as usize] & white
     }
 }
+
+#[cfg(test)]
+mod black_test {
+    use super::*;
+    use crate::square::constants::*;
+
+    #[test]
+    fn test_control() {
+        let (bn, zero) = (BlackKnight, BitBoard::EMPTY);
+        assert_eq!(D1 | C2 | C4 | D5 | F5 | G4 | G2 | F1, bn.controlset(E3, zero, zero));
+        assert_eq!(A4 | C4 | D3 | D1, bn.controlset(B2, zero, zero));
+    }
+
+    #[test]
+    fn test_moveset() {
+        let bn = BlackKnight;
+        assert_eq!(C4 | D3 | D1, bn.moveset(B2, D1 | B1, A4 | D7));
+    }
+
+    #[test]
+    fn test_attackset() {
+        let bn = BlackKnight;
+        assert_eq!(D1.as_set(), bn.attackset(B2, D1 | B1, A4 | D7));
+    }
+}
+
