0518f74bbaa4670dcea4d1ee2730f560446b2ae6
Tom <tomoliverball@gmail.com>
2019-04-03 20:24:30 +0100

    Finally got magic numbers implemented in reasonably performant way

--- corrosion/src/pieces/sliding/mod.rs
+++ corrosion/src/pieces/sliding/mod.rs
@@ -5,9 +5,11 @@ use crate::dir::{E, N, S, W};
 use crate::dir::{NE, NW, SE, SW};
 use crate::square::constants::SQUARES;
 use crate::square::Square;
-use std::collections::HashSet;
-use std::vec::IntoIter;
 use rand::prelude::*;
+use std::collections::HashSet;
+use std::iter::repeat;
+use std::num::Wrapping;
+use std::vec::IntoIter;
 
 /// Computes a bitboard representation of every square
 /// on the edge of a chessboard.
@@ -24,7 +26,7 @@ fn rook_dirs() -> Vec<Dir> {
     vec![N, E, S, W]
 }
 
-/// Computes a vector containing the rook occupancy
+/// Computes a vector containing the occupancy
 /// masks for each square.
 ///
 fn compute_masks(dirs: Vec<Dir>) -> Vec<BitBoard> {
@@ -48,7 +50,6 @@ fn search_remove_last(loc: Square, dir: Dir) -> BitBoard {
     }
     res.into_iter().collect()
 }
-
 
 #[cfg(test)]
 mod mask_tests {
@@ -75,53 +76,12 @@ lazy_static! {
 ///
 ///
 fn compute_shifts(dirs: Vec<Dir>) -> Vec<usize> {
-    compute_masks(dirs)
-        .iter()
-        .map(|x| 64 - x.size())
-        .collect()
+    compute_masks(dirs).iter().map(|x| 64 - x.size()).collect()
 }
 
 lazy_static! {
     static ref BISHOP_SHIFTS: Vec<usize> = compute_shifts(bishop_dirs());
     static ref ROOK_SHIFTS: Vec<usize> = compute_shifts(rook_dirs());
-}
-
-/// Computes the control set for a piece assumed to be
-/// located at a given source square and which is permitted
-/// to move in a specified set of directions.
-///
-fn sliding_control(loc: Square, occ: BitBoard, dirs: Vec<Dir>) -> BitBoard {
-    let mut res = vec![];
-    for dir in dirs {
-        for sq in loc.search_vec(dir) {
-            res.push(sq);
-            if !(occ & sq).is_empty() {
-                break;
-            }
-        }
-    }
-    res.into_iter().collect()
-}
-
-#[cfg(test)]
-mod control_tests {
-    use super::*;
-    use crate::square::constants::*;
-
-    #[test]
-    fn test_sliding_control() {
-        // Could split this up to test rook and bishop separately.
-        let loc = D4;
-        let whites = D1 | F4 | D6 | G7 | H8;
-        let blacks = B2 | B4 | E3 | A7;
-        let dirs = vec![N, NE, E, SE, S, SW, W, NW];
-        let expected_control =
-            D5 | D6 | E5 | F6 | G7 | E4 | F4 | E3 | D3 | D2 | D1 | C3 | B2 | C4 | B4 | C5 | B6 | A7;
-        assert_eq!(
-            expected_control,
-            sliding_control(loc, whites | blacks, dirs)
-        );
-    }
 }
 
 /// Computes the powerset of some set of squares with the
@@ -167,33 +127,99 @@ mod powerset_test {
     }
 }
 
-/// Use brute force trial end error to compute a valid set
-/// of magic numbers
-fn compute_magic_numbers(dirs: Vec<Dir>) -> Vec<usize> {
-    let masks = compute_masks(dirs.clone());
-    let shifts = compute_shifts(dirs.clone());
-    loop {
-        let guess = gen_64_randoms();
-        for (&num, &mask, &shift) in izip!(&guess, &masks, &shifts) {
-            let mut indices = HashSet::new();
-            for occ_var in compute_powerset(&mask.into_iter().collect()) {
-                let index = (occ_var.loc() * (num as u64)) >> shift;
-                // Sanity check
-                // Should I be checking the index is less than the number of the occ vars?
-                assert_eq!(index, (index as usize) as u64);
-                if indices.contains(&index) {
-                    continue;
-                } else {
-                    indices.insert(index);
-                }
+/// Computes the control set for a piece assumed to be
+/// located at a given source square and which is permitted
+/// to move in a specified set of directions.
+///
+fn sliding_control(loc: Square, occ: BitBoard, dirs: &Vec<Dir>) -> BitBoard {
+    let mut res = 0u64;
+    for &dir in dirs {
+        for sq in loc.search_vec(dir) {
+            res |= (1u64 << sq.i);
+            if !(occ & sq).is_empty() {
+                break;
             }
         }
-        // These numbers do not generate any collisions
-        break guess;
+    }
+    BitBoard::wrap(res)
+}
+
+#[cfg(test)]
+mod control_tests {
+    use super::*;
+    use crate::square::constants::*;
+
+    #[test]
+    fn test_sliding_control() {
+        // Could split this up to test rook and bishop separately.
+        let loc = D4;
+        let whites = D1 | F4 | D6 | G7 | H8;
+        let blacks = B2 | B4 | E3 | A7;
+        let dirs = vec![N, NE, E, SE, S, SW, W, NW];
+        let expected_control =
+            D5 | D6 | E5 | F6 | G7 | E4 | F4 | E3 | D3 | D2 | D1 | C3 | B2 | C4 | B4 | C5 | B6 | A7;
+        assert_eq!(
+            expected_control,
+            sliding_control(loc, whites | blacks, &dirs)
+        );
     }
 }
 
-fn gen_64_randoms() -> Vec<usize> {
-    let mut rng = rand::thread_rng();
-    (0..64).map(|x| rng.gen()).collect()
+/// Use brute force trial end error to compute a valid set
+/// of magic numbers
+///
+fn compute_magic_numbers(dirs: Vec<Dir>) -> Vec<u64> {
+    let masks = compute_masks(dirs.clone());
+    let shifts = compute_shifts(dirs.clone());
+    let mut magics: Vec<u64> = Vec::with_capacity(64);
+
+    for (&sq, &mask, &shift) in izip!(SQUARES.iter(), &masks, &shifts) {
+        let occ_vars = compute_powerset(&mask.into_iter().collect());
+        let control: Vec<_> = occ_vars
+            .iter()
+            .map(|&ov| sliding_control(sq, ov, &dirs).loc())
+            .collect();
+        let mut indices: Vec<_> = repeat(064).take(occ_vars.len()).collect();
+        let mut moves: Vec<_> = repeat(064).take(occ_vars.len()).collect();
+        let upper = 100000000;
+        'outer: for i in 1..=upper {
+            let magic = gen_magic_candidate();
+
+            for (&occ_var, &control) in occ_vars.iter().zip(control.iter()) {
+                let (w_occ_var, w_num) = (Wrapping(occ_var.loc()), Wrapping(magic));
+                let index = ((w_occ_var * w_num).0 >> shift) as usize;
+                if indices[index] == i {
+                    if moves[index] != control {
+                        // The magic candidate has failed
+                        continue 'outer;
+                    }
+                } else {
+                    indices[index] = i;
+                    moves[index] = control;
+                }
+            }
+
+            if i == upper {
+                panic!("Failed to generate number!")
+            } else {
+                magics.push(magic);
+                break;
+            }
+        }
+    }
+    magics
+}
+
+fn gen_magic_candidate() -> u64 {
+    rand::random::<u64>() & rand::random::<u64>() & rand::random::<u64>()
+}
+
+#[cfg(test)]
+mod magic_num_test {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(true, compute_magic_numbers(vec![NE, SE, SW, NW]).len() > 0);
+    }
 }
