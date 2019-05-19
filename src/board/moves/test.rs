8650a45c1ec9ad925d8163bd78a0f18df56a7730
Tom <tomoliverball@gmail.com>
2019-05-19 09:56:51 +0100

    Adding move computation tests for each position of tal vs szukszta

--- src/board/moves/test.rs
+++ src/board/moves/test.rs
@@ -20,14 +20,14 @@ type MoveSet = BTreeSet<Move>;
 struct TestCase {
     board: TestBoard,
 
-    expected_castle_moves: Vec<CastleZone>,
-    expected_enpassant_moves: Vec<BitBoard>,
-    expected_promotion_moves: Vec<PrototypeMoveSet>,
-    expected_standard_moves: Vec<PrototypeMoveSet>,
+    castle_moves: Vec<CastleZone>,
+    enpassant_moves: Vec<BitBoard>,
+    promotion_moves: Vec<PrototypeMoveSet>,
+    standard_moves: Vec<PrototypeMoveSet>,
 
-    expected_enpassant_attacks: Vec<BitBoard>,
-    expected_promotion_attacks: Vec<PrototypeMoveSet>,
-    expected_standard_attacks: Vec<PrototypeMoveSet>,
+    enpassant_attacks: Vec<BitBoard>,
+    promotion_attacks: Vec<PrototypeMoveSet>,
+    standard_attacks: Vec<PrototypeMoveSet>,
 }
 
 impl Reflectable for PrototypeMoveSet {
@@ -40,13 +40,13 @@ impl Reflectable for TestCase {
     fn reflect(&self) -> Self {
         TestCase {
             board: self.board.reflect(),
-            expected_castle_moves: self.expected_castle_moves.reflect(),
-            expected_enpassant_moves: self.expected_enpassant_moves.reflect(),
-            expected_promotion_moves: self.expected_promotion_moves.reflect(),
-            expected_standard_moves: self.expected_standard_moves.reflect(),
-            expected_enpassant_attacks: self.expected_enpassant_attacks.reflect(),
-            expected_promotion_attacks: self.expected_promotion_attacks.reflect(),
-            expected_standard_attacks: self.expected_standard_attacks.reflect(),
+            castle_moves: self.castle_moves.reflect(),
+            enpassant_moves: self.enpassant_moves.reflect(),
+            promotion_moves: self.promotion_moves.reflect(),
+            standard_moves: self.standard_moves.reflect(),
+            enpassant_attacks: self.enpassant_attacks.reflect(),
+            promotion_attacks: self.promotion_attacks.reflect(),
+            standard_attacks: self.standard_attacks.reflect(),
         }
     }
 }
@@ -59,7 +59,7 @@ fn convert_case(case: TestCase) -> (Board, MoveSet, MoveSet) {
     let board = case.board.to_board();
 
     let castle_moves = case
-        .expected_castle_moves
+        .castle_moves
         .iter()
         .map(|&zone| Move::Castle(zone))
         .collect::<Vec<_>>();
@@ -71,23 +71,23 @@ fn convert_case(case: TestCase) -> (Board, MoveSet, MoveSet) {
             .collect::<Vec<_>>()
     };
 
-    let enpassant_moves = convert_enpassant(case.expected_enpassant_moves);
-    let promotion_moves = convert_promotion(&board, case.expected_promotion_moves);
-    let standard_moves = convert_standard(&board, case.expected_standard_moves);
+    let enpassant_moves = convert_enpassant(case.enpassant_moves);
+    let promotion_moves = convert_promotion(&board, case.promotion_moves);
+    let standard_moves = convert_standard(&board, case.standard_moves);
 
-    let enpassant_attacks = convert_enpassant(case.expected_enpassant_attacks);
-    let promotion_attacks = convert_promotion(&board, case.expected_promotion_attacks);
-    let standard_attacks = convert_standard(&board, case.expected_standard_attacks);
+    let enpassant_attacks = convert_enpassant(case.enpassant_attacks);
+    let promotion_attacks = convert_promotion(&board, case.promotion_attacks);
+    let standard_attacks = convert_standard(&board, case.standard_attacks);
 
-    let expected_moves = combine(vec![
+    let moves = combine(vec![
         enpassant_moves.clone(),
         castle_moves,
         promotion_moves,
         standard_moves,
     ]);
-    let expected_attacks = combine(vec![enpassant_attacks, promotion_attacks, standard_attacks]);
+    let attacks = combine(vec![enpassant_attacks, promotion_attacks, standard_attacks]);
 
-    (board, expected_moves, expected_attacks)
+    (board, moves, attacks)
 }
 
 fn combine(moves: Vec<Vec<Move>>) -> MoveSet {
@@ -117,13 +117,13 @@ fn execute_test(case: TestCase) {
 }
 
 fn execute_test_impl(case: TestCase) {
-    let (board, expected_moves, expected_attacks) = convert_case(case);
+    let (board, moves, attacks) = convert_case(case);
     let actual_moves = board.compute_moves().into_iter().collect::<BTreeSet<_>>();
     assert_eq!(
-        expected_moves.clone(),
+        moves.clone(),
         actual_moves.clone(),
         "Differences are: {:?}",
-        compute_difference(expected_moves, actual_moves)
+        compute_difference(moves, actual_moves)
     );
 
     let actual_attacks = board
@@ -131,10 +131,10 @@ fn execute_test_impl(case: TestCase) {
         .into_iter()
         .collect::<BTreeSet<_>>();
     assert_eq!(
-        expected_attacks.clone(),
+        attacks.clone(),
         actual_attacks.clone(),
         "Differences are: {:?}",
-        compute_difference(expected_attacks, actual_attacks)
+        compute_difference(attacks, actual_attacks)
     );
 }
 
@@ -162,15 +162,15 @@ fn case_1() {
             enpassant: Some(sq(E6)),
         },
 
-        expected_castle_moves: vec![CastleZone::WQ],
+        castle_moves: vec![CastleZone::WQ],
 
-        expected_enpassant_moves: vec![F5],
-        expected_enpassant_attacks: vec![F5],
+        enpassant_moves: vec![F5],
+        enpassant_attacks: vec![F5],
 
-        expected_promotion_moves: vec![],
-        expected_promotion_attacks: vec![],
+        promotion_moves: vec![],
+        promotion_attacks: vec![],
 
-        expected_standard_moves: vec![
+        standard_moves: vec![
             (A1, A2 | B1 | C1 | D1),
             (A3, B2 | C1 | B4 | C5 | D6 | E7 | F8),
             (B3, B4),
@@ -183,7 +183,7 @@ fn case_1() {
             (H1, G1 | F1 | H2 | H3 | H4 | H5 | H6 | H7),
         ],
 
-        expected_standard_attacks: vec![(C3, B5), (F5, G6), (F3, C6), (H1, H7)],
+        standard_attacks: vec![(C3, B5), (F5, G6), (F3, C6), (H1, H7)],
     });
 }
 
@@ -202,15 +202,15 @@ fn case_2() {
             enpassant: Some(sq(D3)),
         },
 
-        expected_castle_moves: vec![],
+        castle_moves: vec![],
 
-        expected_enpassant_moves: vec![E4],
-        expected_enpassant_attacks: vec![E4],
+        enpassant_moves: vec![E4],
+        enpassant_attacks: vec![E4],
 
-        expected_promotion_moves: vec![],
-        expected_promotion_attacks: vec![],
+        promotion_moves: vec![],
+        promotion_attacks: vec![],
 
-        expected_standard_moves: vec![
+        standard_moves: vec![
             (A8, A7 | A6 | A5 | A4 | A3),
             (B8, A6 | D7),
             (B5, A6 | A4 | C4 | D3 | E2 | F1),
@@ -227,7 +227,7 @@ fn case_2() {
             (H8, G8 | F8),
         ],
 
-        expected_standard_attacks: vec![(E4, F3), (A8, A3)],
+        standard_attacks: vec![(E4, F3), (A8, A3)],
     });
 }
 
@@ -253,15 +253,15 @@ fn case_3() {
             enpassant: None,
         },
 
-        expected_castle_moves: vec![CastleZone::WK],
+        castle_moves: vec![CastleZone::WK],
 
-        expected_enpassant_moves: vec![],
-        expected_enpassant_attacks: vec![],
+        enpassant_moves: vec![],
+        enpassant_attacks: vec![],
 
-        expected_promotion_moves: vec![(A7, A8 | B8)],
-        expected_promotion_attacks: vec![(A7, B8)],
+        promotion_moves: vec![(A7, A8 | B8)],
+        promotion_attacks: vec![(A7, B8)],
 
-        expected_standard_moves: vec![
+        standard_moves: vec![
             (A4, B3 | C2 | D1 | B5 | C6),
             (B1, B2 | B3 | B4 | B5 | B6 | B7 | B8 | A1 | C1 | D1),
             (E1, D1 | D2 | F1),
@@ -271,7 +271,7 @@ fn case_3() {
             (H1, G1 | F1 | H2 | H3 | H4 | H5 | H6 | H7),
         ],
 
-        expected_standard_attacks: vec![(A4, C6), (B1, B8), (F5, G7), (H1, H7)],
+        standard_attacks: vec![(A4, C6), (B1, B8), (F5, G7), (H1, H7)],
     });
 }
 
@@ -297,14 +297,14 @@ fn case_4() {
             enpassant: None,
         },
 
-        expected_castle_moves: vec![CastleZone::BK],
-        expected_enpassant_moves: vec![],
-        expected_enpassant_attacks: vec![],
+        castle_moves: vec![CastleZone::BK],
+        enpassant_moves: vec![],
+        enpassant_attacks: vec![],
 
-        expected_promotion_moves: vec![(A2, A1 | B1)],
-        expected_promotion_attacks: vec![(A2, B1)],
+        promotion_moves: vec![(A2, A1 | B1)],
+        promotion_attacks: vec![(A2, B1)],
 
-        expected_standard_moves: vec![
+        standard_moves: vec![
             (A5, B6 | C7 | D8 | B4 | C3),
             (B8, B7 | B6 | B5 | B4 | B3 | B2 | B1 | A8 | C8 | D8),
             (E8, D7 | D8 | F8),
@@ -315,7 +315,7 @@ fn case_4() {
             (H7, H6 | H5),
         ],
 
-        expected_standard_attacks: vec![(A5, C3), (B8, B1), (F4, G2), (G6, F5)],
+        standard_attacks: vec![(A5, C3), (B8, B1), (F4, G2), (G6, F5)],
     });
 }
 
@@ -334,16 +334,16 @@ fn case_5() {
             enpassant: None,
         },
 
-        expected_castle_moves: vec![],
+        castle_moves: vec![],
 
-        expected_enpassant_moves: vec![],
-        expected_enpassant_attacks: vec![],
+        enpassant_moves: vec![],
+        enpassant_attacks: vec![],
 
-        expected_promotion_moves: vec![],
-        expected_promotion_attacks: vec![],
+        promotion_moves: vec![],
+        promotion_attacks: vec![],
 
-        expected_standard_moves: vec![(F1, G1), (E1, E2), (C2, D3 | E2), (F3, E2), (C3, E2 | B5)],
-        expected_standard_attacks: vec![(F1, G1), (E1, E2), (C2, D3 | E2), (F3, E2), (C3, E2 | B5)],
+        standard_moves: vec![(F1, G1), (E1, E2), (C2, D3 | E2), (F3, E2), (C3, E2 | B5)],
+        standard_attacks: vec![(F1, G1), (E1, E2), (C2, D3 | E2), (F3, E2), (C3, E2 | B5)],
     });
 }
 
@@ -362,22 +362,22 @@ fn case_6() {
             enpassant: None,
         },
 
-        expected_castle_moves: vec![],
+        castle_moves: vec![],
 
-        expected_enpassant_moves: vec![],
-        expected_enpassant_attacks: vec![],
+        enpassant_moves: vec![],
+        enpassant_attacks: vec![],
 
-        expected_promotion_moves: vec![],
-        expected_promotion_attacks: vec![],
+        promotion_moves: vec![],
+        promotion_attacks: vec![],
 
-        expected_standard_moves: vec![
+        standard_moves: vec![
             (E8, F8 | D7 | D8),
             (B5, E2),
             (G6, E4),
             (C7, E5 | E7),
             (F4, E2 | E6),
         ],
-        expected_standard_attacks: vec![
+        standard_attacks: vec![
             (E8, F8 | D7 | D8),
             (B5, E2),
             (G6, E4),
@@ -402,15 +402,238 @@ fn case_7() {
             enpassant: None,
         },
 
-        expected_castle_moves: vec![],
+        castle_moves: vec![],
 
-        expected_enpassant_moves: vec![],
-        expected_enpassant_attacks: vec![],
+        enpassant_moves: vec![],
+        enpassant_attacks: vec![],
 
-        expected_promotion_moves: vec![],
-        expected_promotion_attacks: vec![],
+        promotion_moves: vec![],
+        promotion_attacks: vec![],
 
-        expected_standard_moves: vec![(C3, C4 | C2 | B4 | D2)],
-        expected_standard_attacks: vec![(C3, C4 | C2 | B4 | D2)],
+        standard_moves: vec![(C3, C4 | C2 | B4 | D2)],
+        standard_attacks: vec![(C3, C4 | C2 | B4 | D2)],
     });
+}
+
+#[cfg(test)]
+mod szukstra_vs_tal {
+    use super::*;
+
+    #[test]
+    fn white_move_seven() {
+        execute_test(TestCase {
+            board: TestBoard {
+                active: Side::White,
+                whites: vec![
+                    A2 | B2 | C4 | D4 | E4 | F3 | G2 | H2,
+                    C3 | E2,
+                    E3 | F1,
+                    A1 | H1,
+                    D1,
+                    E1,
+                ],
+                blacks: vec![
+                    A7 | B7 | C7 | D6 | E5 | F7 | G6 | H7,
+                    B8 | F6,
+                    C8 | G7,
+                    A8 | F8,
+                    D8,
+                    G8,
+                ],
+                clock: 1,
+                hash_offset: 14,
+                castle_rights: CastleZoneSet::WHITE,
+                white_status: None,
+                black_status: Some(CastleZone::BK),
+                enpassant: None,
+            },
+
+            castle_moves: vec![],
+            enpassant_moves: vec![],
+            enpassant_attacks: vec![],
+            promotion_moves: vec![],
+            promotion_attacks: vec![],
+            standard_moves: vec![
+                (A1, B1 | C1),
+                (A2, A3 | A4),
+                (B2, B3 | B4),
+                (C3, B1 | A4 | B5 | D5),
+                (C4, C5),
+                (D1, C1 | B1 | C2 | B3 | A4 | D2 | D3),
+                (D4, D5 | E5),
+                (E1, D2 | F2),
+                (E2, C1 | G1 | G3 | F4),
+                (E3, D2 | C1 | F2 | G1 | F4 | G5 | H6),
+                (F3, F4),
+                (G2, G3 | G4),
+                (H1, G1),
+                (H2, H3 | H4),
+            ],
+
+            standard_attacks: vec![(D4, E5)],
+        });
+    }
+
+    #[test]
+    fn black_move_seven() {
+        execute_test(TestCase {
+            board: TestBoard {
+                active: Side::Black,
+                whites: vec![
+                    A2 | B2 | C4 | D4 | E4 | F3 | G2 | H2,
+                    C3 | E2,
+                    E3 | F1,
+                    A1 | H1,
+                    D1,
+                    E1,
+                ],
+                blacks: vec![
+                    A7 | B7 | C6 | D6 | E5 | F7 | G6 | H7,
+                    B8 | F6,
+                    C8 | G7,
+                    A8 | F8,
+                    D8,
+                    G8,
+                ],
+                clock: 0,
+                hash_offset: 15,
+                castle_rights: CastleZoneSet::WHITE,
+                white_status: None,
+                black_status: Some(CastleZone::BK),
+                enpassant: None,
+            },
+
+            castle_moves: vec![],
+            enpassant_moves: vec![],
+            enpassant_attacks: vec![],
+            promotion_moves: vec![],
+            promotion_attacks: vec![],
+            standard_moves: vec![
+                (A7, A6 | A5),
+                (B8, A6 | D7),
+                (B7, B6 | B5),
+                (C8, D7 | E6 | F5 | G4 | H3),
+                (C6, C5),
+                (D8, C7 | B6 | A5 | D7 | E8 | E7),
+                (D6, D5),
+                (E5, D4),
+                (F8, E8),
+                (F6, D5 | E4 | D7 | E8 | H5 | G4),
+                (G8, H8),
+                (G7, H8 | H6),
+                (G6, G5),
+                (H7, H6 | H5),
+            ],
+
+            standard_attacks: vec![(E5, D4), (F6, E4)],
+        });
+    }
+    #[test]
+    fn white_move_eight() {
+        execute_test(TestCase {
+            board: TestBoard {
+                active: Side::White,
+                whites: vec![
+                    A2 | B2 | C4 | D4 | E4 | F3 | G2 | H2,
+                    C3 | E2,
+                    E3 | F1,
+                    A1 | H1,
+                    B3,
+                    E1,
+                ],
+                blacks: vec![
+                    A7 | B7 | C7 | D6 | E5 | F7 | G6 | H7,
+                    B8 | F6,
+                    C8 | G7,
+                    A8 | F8,
+                    D8,
+                    G8,
+                ],
+                clock: 1,
+                hash_offset: 14,
+                castle_rights: CastleZoneSet::WHITE,
+                white_status: None,
+                black_status: Some(CastleZone::BK),
+                enpassant: None,
+            },
+
+            castle_moves: vec![CastleZone::WQ],
+            enpassant_moves: vec![],
+            enpassant_attacks: vec![],
+            promotion_moves: vec![],
+            promotion_attacks: vec![],
+            standard_moves: vec![
+                (A1, B1 | C1 | D1),
+                (A2, A3 | A4),
+                (C3, B1 | A4 | B5 | D5 | D1),
+                (C4, C5),
+                (B3, C2 | D1 | A3 | A4 | B4 | B5 | B6 | B7),
+                (D4, D5 | E5),
+                (E1, D2 | F2 | D1),
+                (E2, C1 | G1 | G3 | F4),
+                (E3, D2 | C1 | F2 | G1 | F4 | G5 | H6),
+                (F3, F4),
+                (G2, G3 | G4),
+                (H1, G1),
+                (H2, H3 | H4),
+            ],
+
+            standard_attacks: vec![(D4, E5), (B3, B7)],
+        });
+    }
+
+//    #[test]
+//    fn black_move_eight() {
+//        execute_test(TestCase {
+//            board: TestBoard {
+//                active: Side::Black,
+//                whites: vec![
+//                    A2 | B2 | C4 | D4 | E4 | F3 | G2 | H2,
+//                    C3 | E2,
+//                    E3 | F1,
+//                    A1 | H1,
+//                    B3,
+//                    E1,
+//                ],
+//                blacks: vec![
+//                    A7 | B7 | C6 | D6 | E5 | F7 | G6 | H7,
+//                    B8 | F6,
+//                    C8 | G7,
+//                    A8 | F8,
+//                    D8,
+//                    G8,
+//                ],
+//                clock: 0,
+//                hash_offset: 15,
+//                castle_rights: CastleZoneSet::WHITE,
+//                white_status: None,
+//                black_status: Some(CastleZone::BK),
+//                enpassant: None,
+//            },
+//
+//            castle_moves: vec![],
+//            enpassant_moves: vec![],
+//            enpassant_attacks: vec![],
+//            promotion_moves: vec![],
+//            promotion_attacks: vec![],
+//            standard_moves: vec![
+//                (A7, A6 | A5),
+//                (B8, A6 | D7),
+//                (B7, B6 | B5),
+//                (C8, D7 | E6 | F5 | G4 | H3),
+//                (C6, C5),
+//                (D8, C7 | B6 | A5 | D7 | E8 | E7),
+//                (D6, D5),
+//                (E5, D4),
+//                (F8, E8),
+//                (F6, D5 | E4 | D7 | E8 | H5 | G4),
+//                (G8, H8),
+//                (G7, H8 | H6),
+//                (G6, G5),
+//                (H7, H6 | H5),
+//            ],
+//
+//            standard_attacks: vec![(E5, D4), (F6, E4)],
+//        });
+//    }
 }
