0dd93abd34aaf7c357cbaaa1f42da3ee1c9c00af
Tom <tomoliverball@gmail.com>
2019-05-23 18:42:45 +0100

    Getting to grips with the web frontend

--- www/index.js
+++ www/index.js
@@ -1,3 +1,22 @@
-import * as wasm from "hello-wasm-pack";
+let canvas = document.getElementById("board-canvas");
+const ctx = canvas.getContext("2d");
+let windowWidth = () => window.innerWidth;
+let windowHeight = () => window.innerHeight;
 
-wasm.greet();
+function clearCanvas(canvas) {
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
+let renderOp = () => {
+    clearCanvas(canvas)
+    let w = windowWidth();
+    let h = windowHeight();
+    canvas.width = w;
+    canvas.height = h;
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, w, h);
+    requestAnimationFrame(renderOp);
+}
+
+requestAnimationFrame(renderOp);
