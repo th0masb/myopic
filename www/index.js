e935cb863a25705ee8977d484beab64081b8afff
Tom <tomoliverball@gmail.com>
2019-05-29 18:38:15 +0100

    Got the board rendered

--- www/index.js
+++ www/index.js
@@ -1,14 +1,20 @@
 'use-strict';
 
-import {computeSquareLocations} from './module.js';
+import {computeBoardGeometry} from './module.js';
 
 let canvas = document.getElementById("board-canvas");
 const ctx = canvas.getContext("2d");
+ctx.drawBounds = b => ctx.fillRect(b.minx, b.miny, b.width, b.height);
 let windowWidth = () => window.innerWidth;
 let windowHeight = () => window.innerHeight;
 
 function clearCanvas(canvas) {
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function isLightIndex(index) {
+	let [row, col] = [Math.floor(index / 8), index % 8];
+	return (row + col) % 2 === 0; 
 }
 
 
@@ -19,8 +25,18 @@ let renderOp = () => {
     canvas.width = w;
     canvas.height = h;
     ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, w, h);
+    //ctx.fillRect(0, 0, w, h);
+
+    let [lightFill, darkFill] = ["#e5c9ae", "#442100"]
+    let geometry = computeBoardGeometry(w, h, 50, "w");
+    ctx.fillStyle = darkFill;
+    ctx.drawBounds(geometry.back);
+    ctx.fillStyle = lightFill;
+    for (var i = 0; i < 64; i++) {
+    	if (isLightIndex(i)) {
+    		ctx.drawBounds(geometry.squares[i]);
+    	}
+    }
     requestAnimationFrame(renderOp);
 }
-console.log(computeSquareLocations(560, 300, 10, "w"))
 requestAnimationFrame(renderOp);
--- www/module.js
+++ www/module.js
@@ -1,21 +1,23 @@
 'use-strict';
 
-export function computeSquareLocations(containerWidth, containerHeight, sizeCap, orientation) {
-	if ([containerWidth, containerHeight, sizeCap, orientation].some(x => x === undefined)) {
+export function computeBoardGeometry(containerWidth, containerHeight, sizeCap, orientation) {
+	if ([...arguments].some(x => x === undefined)) {
 		throw "Look at your call args"
 	}
-	let squareSize = Math.min(containerWidth / 8, containerHeight / 8, sizeCap);
-	let boardSize = 8 * squareSize;
-	let [originx, originy] = [(containerWidth - boardSize) / 2, (containerHeight - boardSize) / 2];
+	let squareSize = Math.min(containerWidth / 9, containerHeight / 9, sizeCap);
+	let boardSize = 9 * squareSize;
+	let [originx, originy] = [(containerWidth - boardSize ) / 2, (containerHeight - boardSize) / 2];
 	let dest = [];
+	const squareBounds = (minx, miny) => bounds(minx, miny, squareSize, squareSize);
 	for (var i = 0; i < 64; i++) {
-		let [row, col] = [Math.floor(i / 8), i % 8];
-		dest.push(bounds(originx + col * squareSize, originy + row * squareSize, squareSize, squareSize));
+		let [row, col, sz] = [Math.floor(i / 8), i % 8];
+		dest.push(squareBounds(originx + (col + 0.5) * squareSize, originy + (row + 0.5) * squareSize));
 	}
 	if (orientation === "w") {
 		dest.reverse();
 	}
-	return dest;
+
+	return {squares: dest, back: bounds(originx, originy, boardSize, boardSize)};
 }
 
 function bounds(minx, miny, width, height) {
