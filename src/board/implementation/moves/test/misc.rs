544995ad427161433f31c3d8a103ccf85e46fe74
Tom <tomoliverball@gmail.com>
2019-06-12 19:29:23 +0100

    Refactored case 2

--- src/board/implementation/moves/test/misc.rs
+++ src/board/implementation/moves/test/misc.rs
@@ -28,7 +28,7 @@ fn case_1() {
             s(Piece::WP, G2, G3 | G4),
             s(Piece::WR, H1, G1 | F1 | H2 | H3 | H4 | H5 | H6 | H7),
             e(F5),
-            c(CastleZone::WQ.lift())
+            c(CastleZone::WQ.lift()),
         ],
 
         expected_attacks: vec![
@@ -52,67 +52,19 @@ fn case_1() {
     });
 }
 
-//#[test]
+#[test]
 fn case_2() {
     execute_test(TestCase {
         board: TestBoard {
-            active: Side::Black,
-            whites: vec![B3 | D4 | F2 | G2, C3, A3 | F3, A1 | H1, C2, E1],
-            blacks: vec![C6 | E4 | F7 | G7 | H7, B8, B5 | G6, A8 | H8, C7, E8],
-            clock: 20,
-            hash_offset: 20,
-            castle_rights: CastleZoneSet::ALL,
-            white_status: None,
-            black_status: None,
-            enpassant: Some(sq(D3)),
-        },
-
-        expected_all: vec![],
-        expected_attacks: vec![],
-        expected_attacks_checks: vec![],
-
-//        castle_moves: vec![],
-//
-//        enpassant_moves: vec![E4],
-//        enpassant_attacks: vec![E4],
-//
-//        promotion_moves: vec![],
-//        promotion_attacks: vec![],
-//
-//        standard_moves: vec![
-//            (A8, A7 | A6 | A5 | A4 | A3),
-//            (B8, A6 | D7),
-//            (B5, A6 | A4 | C4 | D3 | E2 | F1),
-//            (
-//                C7,
-//                C8 | D8 | D7 | E7 | D6 | E5 | F4 | G3 | H2 | B6 | A5 | B7 | A7,
-//            ),
-//            (C6, C5),
-//            (E8, D8 | D7),
-//            (E4, E3 | F3),
-//            (F7, F6 | F5),
-//            (G6, H5 | F5),
-//            (H7, H6 | H5),
-//            (H8, G8 | F8),
-//        ],
-//
-//        standard_attacks: vec![(E4, F3), (A8, A3)],
-    });
-}
-
-//#[test]
-fn case_3() {
-    execute_test(TestCase {
-        board: TestBoard {
             active: Side::White,
-            whites: vec![A7 | D4 | F2 | G2, F5 | C3, A4, B1 | H1, BitBoard::EMPTY, E1],
+            whites: vec![A7 | D4 | F2 | G2, C3, A4, B1 | H1, BitBoard::EMPTY, E1],
             blacks: vec![
                 A2 | D5 | F7 | G7 | H7,
-                F4 | C6,
+                C6,
                 G6 | A5,
                 B8 | H8,
                 BitBoard::EMPTY,
-                E8,
+                C7,
             ],
             clock: 20,
             hash_offset: 20,
@@ -122,29 +74,34 @@ fn case_3() {
             enpassant: None,
         },
 
-        expected_all: vec![],
-        expected_attacks: vec![],
-        expected_attacks_checks: vec![],
+        expected_all: vec![
+            s(Piece::WB, A4, B3 | C2 | D1 | B5 | C6),
+            s(
+                Piece::WR,
+                B1,
+                B2 | B3 | B4 | B5 | B6 | B7 | B8 | A1 | C1 | D1,
+            ),
+            s(Piece::WK, E1, E2 | D1 | D2 | F1),
+            s(Piece::WP, F2, F3 | F4),
+            s(Piece::WP, G2, G3 | G4),
+            s(Piece::WR, H1, G1 | F1 | H2 | H3 | H4 | H5 | H6 | H7),
+            c(CastleZone::WK.lift()),
+            p(Side::White, A7, A8 | B8),
+        ],
 
-//        castle_moves: vec![CastleZone::WK],
-//
-//        enpassant_moves: vec![],
-//        enpassant_attacks: vec![],
-//
-//        promotion_moves: vec![(A7, A8 | B8)],
-//        promotion_attacks: vec![(A7, B8)],
-//
-//        standard_moves: vec![
-//            (A4, B3 | C2 | D1 | B5 | C6),
-//            (B1, B2 | B3 | B4 | B5 | B6 | B7 | B8 | A1 | C1 | D1),
-//            (E1, D1 | D2 | F1),
-//            (F2, F3),
-//            (G2, G3 | G4),
-//            (F5, E3 | G3 | H4 | H6 | G7 | E7 | D6),
-//            (H1, G1 | F1 | H2 | H3 | H4 | H5 | H6 | H7),
-//        ],
-//
-//        standard_attacks: vec![(A4, C6), (B1, B8), (F5, G7), (H1, H7)],
+        expected_attacks: vec![
+            s(Piece::WB, A4, C6),
+            s(Piece::WR, B1, B8),
+            s(Piece::WR, H1, H7),
+            p(Side::White, A7, B8),
+        ],
+
+        expected_attacks_checks: vec![
+            s(Piece::WB, A4, C6),
+            s(Piece::WR, B1, B7 | B8),
+            s(Piece::WR, H1, H7),
+            p(Side::White, A7, B8),
+        ],
     });
 }
 
@@ -173,27 +130,25 @@ fn case_4() {
         expected_all: vec![],
         expected_attacks: vec![],
         expected_attacks_checks: vec![],
-
-
-//        castle_moves: vec![CastleZone::BK],
-//        enpassant_moves: vec![],
-//        enpassant_attacks: vec![],
-//
-//        promotion_moves: vec![(A2, A1 | B1)],
-//        promotion_attacks: vec![(A2, B1)],
-//
-//        standard_moves: vec![
-//            (A5, B6 | C7 | D8 | B4 | C3),
-//            (B8, B7 | B6 | B5 | B4 | B3 | B2 | B1 | A8 | C8 | D8),
-//            (E8, D7 | D8 | F8),
-//            (F7, F6),
-//            (F4, D3 | E2 | G2 | H3 | H5 | E6),
-//            (G6, F5 | H5),
-//            (H8, G8 | F8),
-//            (H7, H6 | H5),
-//        ],
-//
-//        standard_attacks: vec![(A5, C3), (B8, B1), (F4, G2), (G6, F5)],
+        //        castle_moves: vec![CastleZone::BK],
+        //        enpassant_moves: vec![],
+        //        enpassant_attacks: vec![],
+        //
+        //        promotion_moves: vec![(A2, A1 | B1)],
+        //        promotion_attacks: vec![(A2, B1)],
+        //
+        //        standard_moves: vec![
+        //            (A5, B6 | C7 | D8 | B4 | C3),
+        //            (B8, B7 | B6 | B5 | B4 | B3 | B2 | B1 | A8 | C8 | D8),
+        //            (E8, D7 | D8 | F8),
+        //            (F7, F6),
+        //            (F4, D3 | E2 | G2 | H3 | H5 | E6),
+        //            (G6, F5 | H5),
+        //            (H8, G8 | F8),
+        //            (H7, H6 | H5),
+        //        ],
+        //
+        //        standard_attacks: vec![(A5, C3), (B8, B1), (F4, G2), (G6, F5)],
     });
 }
 
@@ -215,17 +170,16 @@ fn case_5() {
         expected_all: vec![],
         expected_attacks: vec![],
         expected_attacks_checks: vec![],
-
-//        castle_moves: vec![],
-//
-//        enpassant_moves: vec![],
-//        enpassant_attacks: vec![],
-//
-//        promotion_moves: vec![],
-//        promotion_attacks: vec![],
-//
-//        standard_moves: vec![(F1, G1), (E1, E2), (C2, D3 | E2), (F3, E2), (C3, E2 | B5)],
-//        standard_attacks: vec![(F1, G1), (E1, E2), (C2, D3 | E2), (F3, E2), (C3, E2 | B5)],
+        //        castle_moves: vec![],
+        //
+        //        enpassant_moves: vec![],
+        //        enpassant_attacks: vec![],
+        //
+        //        promotion_moves: vec![],
+        //        promotion_attacks: vec![],
+        //
+        //        standard_moves: vec![(F1, G1), (E1, E2), (C2, D3 | E2), (F3, E2), (C3, E2 | B5)],
+        //        standard_attacks: vec![(F1, G1), (E1, E2), (C2, D3 | E2), (F3, E2), (C3, E2 | B5)],
     });
 }
 
@@ -247,29 +201,28 @@ fn case_6() {
         expected_all: vec![],
         expected_attacks: vec![],
         expected_attacks_checks: vec![],
-
-//        castle_moves: vec![],
-//
-//        enpassant_moves: vec![],
-//        enpassant_attacks: vec![],
-//
-//        promotion_moves: vec![],
-//        promotion_attacks: vec![],
-//
-//        standard_moves: vec![
-//            (E8, F8 | D7 | D8),
-//            (B5, E2),
-//            (G6, E4),
-//            (C7, E5 | E7),
-//            (F4, E2 | E6),
-//        ],
-//        standard_attacks: vec![
-//            (E8, F8 | D7 | D8),
-//            (B5, E2),
-//            (G6, E4),
-//            (C7, E5 | E7),
-//            (F4, E2 | E6),
-//        ],
+        //        castle_moves: vec![],
+        //
+        //        enpassant_moves: vec![],
+        //        enpassant_attacks: vec![],
+        //
+        //        promotion_moves: vec![],
+        //        promotion_attacks: vec![],
+        //
+        //        standard_moves: vec![
+        //            (E8, F8 | D7 | D8),
+        //            (B5, E2),
+        //            (G6, E4),
+        //            (C7, E5 | E7),
+        //            (F4, E2 | E6),
+        //        ],
+        //        standard_attacks: vec![
+        //            (E8, F8 | D7 | D8),
+        //            (B5, E2),
+        //            (G6, E4),
+        //            (C7, E5 | E7),
+        //            (F4, E2 | E6),
+        //        ],
     });
 }
 
@@ -291,17 +244,16 @@ fn case_7() {
         expected_all: vec![],
         expected_attacks: vec![],
         expected_attacks_checks: vec![],
-
-//        castle_moves: vec![],
-//
-//        enpassant_moves: vec![],
-//        enpassant_attacks: vec![],
-//
-//        promotion_moves: vec![],
-//        promotion_attacks: vec![],
-//
-//        standard_moves: vec![(C3, C4 | C2 | B4 | D2)],
-//        standard_attacks: vec![(C3, C4 | C2 | B4 | D2)],
+        //        castle_moves: vec![],
+        //
+        //        enpassant_moves: vec![],
+        //        enpassant_attacks: vec![],
+        //
+        //        promotion_moves: vec![],
+        //        promotion_attacks: vec![],
+        //
+        //        standard_moves: vec![(C3, C4 | C2 | B4 | D2)],
+        //        standard_attacks: vec![(C3, C4 | C2 | B4 | D2)],
     });
 }
 
@@ -323,17 +275,16 @@ fn case_8() {
         expected_all: vec![],
         expected_attacks: vec![],
         expected_attacks_checks: vec![],
-
-//        castle_moves: vec![],
-//
-//        enpassant_moves: vec![D5, F5],
-//        enpassant_attacks: vec![D5, F5],
-//
-//        promotion_moves: vec![],
-//        promotion_attacks: vec![],
-//
-//        standard_moves: vec![(D5, D6), (F5, F6), (H1, G1 | G2 | H2)],
-//        standard_attacks: vec![],
+        //        castle_moves: vec![],
+        //
+        //        enpassant_moves: vec![D5, F5],
+        //        enpassant_attacks: vec![D5, F5],
+        //
+        //        promotion_moves: vec![],
+        //        promotion_attacks: vec![],
+        //
+        //        standard_moves: vec![(D5, D6), (F5, F6), (H1, G1 | G2 | H2)],
+        //        standard_attacks: vec![],
     });
 }
 
@@ -355,17 +306,16 @@ fn case_9() {
         expected_all: vec![],
         expected_attacks: vec![],
         expected_attacks_checks: vec![],
-
-//        castle_moves: vec![],
-//
-//        enpassant_moves: vec![],
-//        enpassant_attacks: vec![],
-//
-//        promotion_moves: vec![(G7, G8 | H8)],
-//        promotion_attacks: vec![(G7, H8)],
-//
-//        standard_moves: vec![(C5, C6), (B7, B8 | C8 | A8 | A7 | A6 | B6 | C6)],
-//        standard_attacks: vec![],
+        //        castle_moves: vec![],
+        //
+        //        enpassant_moves: vec![],
+        //        enpassant_attacks: vec![],
+        //
+        //        promotion_moves: vec![(G7, G8 | H8)],
+        //        promotion_attacks: vec![(G7, H8)],
+        //
+        //        standard_moves: vec![(C5, C6), (B7, B8 | C8 | A8 | A7 | A6 | B6 | C6)],
+        //        standard_attacks: vec![],
     });
 }
 
@@ -387,16 +337,15 @@ fn case_10() {
         expected_all: vec![],
         expected_attacks: vec![],
         expected_attacks_checks: vec![],
-
-//        castle_moves: vec![],
-//
-//        enpassant_moves: vec![],
-//        enpassant_attacks: vec![],
-//
-//        promotion_moves: vec![],
-//        promotion_attacks: vec![],
-//
-//        standard_moves: vec![(B7, B6 | A6 | A7 | A8 | B8 | C8 | C7)],
-//        standard_attacks: vec![],
+        //        castle_moves: vec![],
+        //
+        //        enpassant_moves: vec![],
+        //        enpassant_attacks: vec![],
+        //
+        //        promotion_moves: vec![],
+        //        promotion_attacks: vec![],
+        //
+        //        standard_moves: vec![(B7, B6 | A6 | A7 | A8 | B8 | C8 | C7)],
+        //        standard_attacks: vec![],
     });
 }
