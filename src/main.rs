b76ed2ef0cf094e437f0a7c62fc0276168c337ae
Tom <tomoliverball@gmail.com>
2019-07-29 19:09:48 +0100

    Refreshed dependency files

--- src/main.rs
+++ src/main.rs
@@ -16,23 +16,21 @@ mod board;
 mod utils;
 mod eval;
 
-use wasm_bindgen::prelude::*;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, jenjinn3tmp!");
-}
+//// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+//// allocator.
+//#[cfg(feature = "wee_alloc")]
+//#[global_allocator]
+//static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+//
+//#[wasm_bindgen]
+//extern {
+//    fn alert(s: &str);
+//}
+//
+//#[wasm_bindgen]
+//pub fn greet() {
+//    alert("Hello, jenjinn3tmp!");
+//}
 
 #[derive(Copy, Clone)]
 enum Test {
