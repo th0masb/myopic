e8fab3815ab6e2eb2f2a0640618c3d49f2f68d06
Tom <tomoliverball@gmail.com>
2019-08-15 20:41:17 +0100

    Continuing termination implementation

--- src/board/implementation/utils/termination.rs
+++ src/board/implementation/utils/termination.rs
@@ -3,14 +3,36 @@ use crate::board::Termination;
 use crate::board::Board;
 use crate::board::implementation::utils::control;
 use crate::base::Reflectable;
+use crate::base::bitboard::BitBoard;
+use crate::base::square::Square;
+use crate::pieces::Piece;
 
 
 impl BoardImpl {
     pub fn compute_termination(&self) -> Option<Termination> {
+        if self.half_move_clock() >= 50 || self.hashes.has_three_repetitions() {
+            return Some(Termination::Draw);
+        }
         let (active, passive) = (self.active, self.active.reflect());
         let active_king = self.king(active);
         let passive_control = self.compute_control(passive);
+        let (whites, blacks) = self.sides();
+        // If king can move somewhere then not terminal.
+        let king_moves = Piece::king(active).moves(active_king, whites, blacks);
+        if !(king_moves - passive_control).is_empty() {
+            None
+        } else if passive_control.contains(active_king) {
+            self.checked_termination(passive_control, active_king)
+        } else {
+            self.unchecked_termination(passive_control, active_king)
+        }
+    }
 
+    fn checked_termination(&self, passive_ctrl: BitBoard, king: Square) -> Option<Termination> {
+        unimplemented!()
+    }
+
+    fn unchecked_termination(&self, passive_ctrl: BitBoard, king: Square) -> Option<Termination> {
         unimplemented!()
     }
 }
