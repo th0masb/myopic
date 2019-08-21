2f691ca0e7536be3f3de9841940c2d279b25b210
Tom <tomoliverball@gmail.com>
2019-08-21 17:46:34 +0100

    Continuing implementation of pgn parsing

--- src/pgn/mod.rs
+++ src/pgn/mod.rs
@@ -1,10 +1,12 @@
 use crate::base::castlezone::CastleZone;
+use crate::base::square::Square;
 use crate::base::Side;
-use crate::board::{Board, BoardImpl, Move};
+use crate::board::Move::Castle;
+use crate::board::{Board, BoardImpl, Move, MoveComputeType};
 use crate::regex::Regex;
 use patterns::*;
 use std::str::FromStr;
-use crate::board::Move::Castle;
+use crate::pieces::Piece;
 
 mod patterns;
 
@@ -30,7 +32,60 @@ fn parse_single_move<B: Board>(start: &mut B, pgn_move: String) -> Result<Move, 
 }
 
 fn parse_standard<B: Board>(start: &mut B, pgn_move: String) -> Result<Move, String> {
-    unimplemented!()
+    let legal = start.compute_moves(MoveComputeType::All);
+    let square_matches: Vec<_> = find_matches(&pgn_move, square_regex())
+        .into_iter()
+        .map(|s| Square::from_string(&s))
+        .collect();
+    if square_matches.len() == 0 {
+        Err(pgn_move)
+    } else if square_matches.len() == 2 {
+        // Rare case of two squares specified we can search straight away.
+        let (start, end) = (square_matches[0], square_matches[1]);
+        let matched_move = legal.iter().find(|&mv| match mv {
+            &Move::Standard(_, s, e) => s == start && e == end,
+            _ => false,
+        });
+        matched_move.map(|mv| mv.clone()).ok_or(pgn_move)
+    } else {
+        let end = square_matches[0];
+        let file = find_differentiating_rank_or_file(&pgn_move, file_regex());
+        let ranks = find_differentiating_rank_or_file(&pgn_move, rank_regex());
+        let piece_ordinal = piece_ordinal(&pgn_move);
+        let piece_matches = |p: Piece| piece_ordinal == (p as usize % 6);
+
+
+        unimplemented!()
+    }
+}
+
+fn char_at(string: &String, index: usize) -> char {
+    string.chars().nth(index).unwrap()
+}
+
+fn find_differentiating_rank_or_file(pgn_move: &String, re: &Regex) -> Option<char> {
+    let all_matches = find_matches(pgn_move, re);
+    if all_matches.len() == 1 {
+        None
+    } else {
+        Some(char_at(&all_matches[0], 0))
+    }
+}
+
+
+fn piece_ordinal(pgn_move: &String) -> usize {
+    let piece = find_matches(&pgn_move, piece_regex())
+        .first()
+        .and_then(|s| s.chars().nth(0));
+    match piece {
+        None => 0,
+        Some('N') => 1,
+        Some('B') => 2,
+        Some('R') => 3,
+        Some('Q') => 4,
+        Some('K') => 5,
+        _ => panic!()
+    }
 }
 
 fn parse_promotion<B: Board>(start: &mut B, pgn_move: String) -> Result<Move, String> {
