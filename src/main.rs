7827da27cc4f1aa5f37b56102c49ae359cf4c2f9
Tom <tomoliverball@gmail.com>
2019-09-04 19:09:08 +0100

    Working on uci interface

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,15 @@ extern crate lazy_static;
 extern crate rand;
 extern crate regex;
 
+use std::fs;
+use std::io::{self, Read};
+use std::path::Path;
+use std::sync::mpsc;
+use std::thread;
+use std::time::{Duration, Instant};
+
+use crate::board::{BoardImpl, Move};
+
 mod base;
 mod eval;
 mod pgn;
@@ -14,27 +23,60 @@ mod pieces;
 mod search;
 mod board;
 
-use std::fs;
-use std::path::Path;
-use std::io::{self, Read};
-use std::thread;
-use std::sync::mpsc;
-use std::time::{Instant, Duration};
+#[derive(Debug)]
+enum EngineState {
+    Initializing,
+    WaitingForPosition,
+    WaitingForGo,
+    Searching,
+    Pondering
+}
+
+#[derive(Debug)]
+enum EngineCmd {
+    Uci,
+    IsReady,
+    UciNewGame,
+    Stop,
+    PonderHit,
+    Quit,
+    Position(String, Vec<String>),
+    Go(Vec<GoCommand>),
+}
+
+#[derive(Debug)]
+enum GoCommand {
+    SearchMoves(Vec<String>),
+    Depth(usize),
+    MoveTime(usize),
+    WhiteTime(usize),
+    BlackTime(usize),
+    WhiteInc(usize),
+    BlackInc(usize),
+    Ponder,
+    Infinite,
+}
+
+fn parse_engine_command(content: String) -> Option<EngineCmd> {
+    unimplemented!()
+}
 
 fn main() {
-    //println!("{:?}", 1u64.trailing_zeros())
-    let (stdin_tx, stdin_rx) = mpsc::channel::<String>();
-
+    let (input_tx, input_rx) = mpsc::channel::<EngineCmd>();
     // Spawn the user input thread, it simply listens for
-    // standard input and transmits the result to the main
+    // standard input, parses the string to an engine command
+    // and transmits the result (if valid) to the main
     // control thread.
     thread::spawn(move || {
         loop {
             let mut dest = String::new();
             io::stdin().read_line(&mut dest);
-            match stdin_tx.send(dest) {
-                Ok(_) => (),
-                Err(_) => continue,
+            let cmd = parse_engine_command(dest);
+            if cmd.is_some() {
+                match input_tx.send(cmd.unwrap()) {
+                    Ok(_) => (),
+                    Err(_) => continue,
+                }
             }
         }
     });
@@ -44,19 +86,11 @@ fn main() {
         if timer.elapsed().as_secs() >= 60 {
             break;
         }
-        match stdin_rx.try_recv() {
-            Ok(input) => println!("Recieved: {}", input),
+        match input_rx.try_recv() {
+            Ok(input) => println!("Recieved: {:?}", input),
             Err(_) => thread::sleep(Duration::from_millis(10)),
         }
     }
 
     println!("quitting");
-
-   // let fen = "rn3k2/pR2b3/4p1Q1/2q1N2P/3R2P1/3K4/P3Br2/8 w - - 1 0";
-   // let fen = "rR3k2/p3b3/4p1Q1/2q1N2P/3R2P1/3K4/P3Br2/8 b - - 0 1";
-   // // No moves computed here!!
-   // let mut board = eval::new_board(fen).unwrap();
-   // let neg = search::best_move(&mut board, 3);
-   // //let neg = search::negamax(&mut board, -eval::INFTY, eval::INFTY, 4);
-   // println!("{:?}", neg);
 }
