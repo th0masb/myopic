dcb0eee31df1b430b813571f967a6990ce8db998
Tom <tomoliverball@gmail.com>
2019-09-04 17:33:16 +0100

    Started working on the uci interface

--- src/main.rs
+++ src/main.rs
@@ -18,17 +18,40 @@ use std::fs;
 use std::path::Path;
 use std::io::{self, Read};
 use std::thread;
+use std::sync::mpsc;
+use std::time::{Instant, Duration};
 
 fn main() {
     //println!("{:?}", 1u64.trailing_zeros())
-    let stdin = io::stdin();
-    let mut dest = String::new();
-    let handle = thread::spawn(move || {
-        stdin.read_line(&mut dest);
-        println!("{}", dest);
+    let (stdin_tx, stdin_rx) = mpsc::channel::<String>();
 
+    // Spawn the user input thread, it simply listens for
+    // standard input and transmits the result to the main
+    // control thread.
+    thread::spawn(move || {
+        loop {
+            let mut dest = String::new();
+            io::stdin().read_line(&mut dest);
+            match stdin_tx.send(dest) {
+                Ok(_) => (),
+                Err(_) => continue,
+            }
+        }
     });
-    handle.join().unwrap();
+
+    let timer = Instant::now();
+    loop {
+        if timer.elapsed().as_secs() >= 60 {
+            break;
+        }
+        match stdin_rx.try_recv() {
+            Ok(input) => println!("Recieved: {}", input),
+            Err(_) => thread::sleep(Duration::from_millis(10)),
+        }
+    }
+
+    println!("quitting");
+
    // let fen = "rn3k2/pR2b3/4p1Q1/2q1N2P/3R2P1/3K4/P3Br2/8 w - - 1 0";
    // let fen = "rR3k2/p3b3/4p1Q1/2q1N2P/3R2P1/3K4/P3Br2/8 b - - 0 1";
    // // No moves computed here!!
