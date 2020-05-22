606721a695ff9cf712eaa495af315598bdd596df
Thomas Ball <tomoliverball@gmail.com>
2020-05-22 14:08:58 +0100

    Started adding more stages to pipeline

--- ci/rust-lib.groovy
+++ ci/rust-lib.groovy
@@ -1,4 +1,15 @@
-stage("First stage") {
-    println("I'm alive!!")
-    println("I injected the variable ${BRANCH} from the remote call!")
+
+node {
+    stage("Update toolchain") {
+        sh "rustup update"
+    }
+
+    stage("Clone Repository") {
+        echo pwd()
+    }
+
+    stage("Apply linting") {
+        println("I'm alive!!")
+        println("I injected the variable ${BRANCH} from the remote call!")
+    }
 }
