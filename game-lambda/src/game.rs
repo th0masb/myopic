7ab1b6b74b9d69fb71c1ec7e9ec6ae1b896b4815
Thomas Ball <tomoliverball@gmail.com>
2020-10-13 11:48:53 +0100

    [MYO-16] Implemented basic game-lambda

---
+++ game-lambda/src/game.rs
@@ -0,0 +1,149 @@
+use crate::events::{Clock, GameEvent, GameFull, GameState, ChatLine};
+use crate::helper::*;
+use myopic_board::{parse, Move, MutBoard};
+use myopic_core::Side;
+use reqwest::blocking::Client;
+use std::time::Duration;
+
+const MOVE_ENDPOINT: &'static str = "https://lichess.org/api/bot/game";
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct GameMetadata {
+    lambda_side: Side,
+    clock: Clock,
+}
+
+#[derive(Debug)]
+pub struct Game {
+    id: String,
+    lambda_player_id: String,
+    expected_half_moves: u32,
+    metadata: Vec<GameMetadata>,
+    auth_token: String,
+    client: Client,
+    is_finished: bool,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+pub enum GameExecutionState {
+    Running,
+    Finished,
+}
+
+impl Game {
+    pub fn new(
+        id: String,
+        lambda_player_id: String,
+        expected_half_moves: u32,
+        auth_token: &str,
+    ) -> Game {
+        Game {
+            id,
+            lambda_player_id,
+            expected_half_moves,
+            metadata: Vec::new(),
+            auth_token: auth_token.to_owned(),
+            client: reqwest::blocking::Client::new(),
+            is_finished: false,
+        }
+    }
+
+    pub fn process_event(&mut self, event_json: &str) -> Result<GameExecutionState, String> {
+        match serde_json::from_str(event_json) {
+            Err(error) => {
+                log::warn!("Error parsing event {}", error);
+                Err(format!("{}", error))
+            },
+            Ok(event) => match event {
+                GameEvent::GameFull { content } => {
+                    log::info!("Parsed full game information");
+                    self.process_game_full(content)
+                },
+                GameEvent::State { content } => {
+                    log::info!("Parsed individual game state");
+                    self.process_game_state(content)
+                },
+                GameEvent::ChatLine { content } => {
+                    log::info!("Parsed chat line");
+                    self.process_chat_line(content)
+                },
+            },
+        }
+    }
+
+    fn process_chat_line(&self, _chat_line: ChatLine) -> Result<GameExecutionState, String> {
+        // Do nothing for now
+        if self.is_finished {
+            Ok(GameExecutionState::Finished)
+        } else {
+            Ok(GameExecutionState::Running)
+        }
+    }
+
+    fn process_game_full(&mut self, game_full: GameFull) -> Result<GameExecutionState, String> {
+        // Track info required for playing future gamestates
+        self.metadata.push(GameMetadata {
+            clock: game_full.clock,
+            lambda_side: if self.lambda_player_id == game_full.white.id {
+                log::info!("Detected lambda is playing as white");
+                Side::White
+            } else if self.lambda_player_id == game_full.black.id {
+                log::info!("Detected lambda is playing as black");
+                Side::Black
+            } else {
+                return Err(format!("Unrecognized names"));
+            },
+        });
+        self.process_game_state(game_full.state)
+    }
+
+    fn process_game_state(&mut self, state: GameState) -> Result<GameExecutionState, String> {
+        let metadata = self.get_latest_metadata()?;
+        log::info!("Parsing previous game moves: {}", state.moves);
+        let moves = parse::uci(&state.moves)?;
+        let mut board = myopic_brain::eval::start();
+        moves.iter().for_each(|mv| {
+            board.evolve(mv);
+        });
+
+        match board.termination_status() {
+            Some(_) => {
+                log::info!("Game has finished! Terminating execution");
+                self.is_finished = true;
+                Ok(GameExecutionState::Finished)
+            },
+            None => {
+                self.is_finished = false;
+                if board.active() != metadata.lambda_side {
+                    log::info!("It is not our turn, waiting for opponents move");
+                    Ok(GameExecutionState::Running)
+                } else {
+                    let thinking_time = compute_thinking_time(ThinkingTimeParams {
+                        expected_half_move_count: self.expected_half_moves,
+                        half_moves_played: moves.len() as u32,
+                        initial: Duration::from_millis(metadata.clock.initial),
+                        increment: Duration::from_millis(metadata.clock.increment),
+                    });
+                    log::info!("Computed we should spend {}s thinking", thinking_time.as_secs());
+                    let result = myopic_brain::search(board, thinking_time)?;
+                    log::info!("Completed search: {:?}", result);
+                    self.post_move(result.best_move)
+                }
+            }
+        }
+    }
+
+    fn get_latest_metadata(&self) -> Result<GameMetadata, String> {
+        self.metadata.get(0).cloned().ok_or(format!("Metadata not initialized"))
+    }
+
+    fn post_move(&self, mv: Move) -> Result<GameExecutionState, String> {
+        // Add timeout and retry logic
+        self.client
+            .post(format!("{}/{}/move/{}", MOVE_ENDPOINT, self.id, move_to_uci(&mv)).as_str())
+            .bearer_auth(&self.auth_token)
+            .send()
+            .map(|_| GameExecutionState::Running)
+            .map_err(|error| format!("Error posting move: {}", error))
+    }
+}
---
+++ game-lambda/src/main.rs
@@ -0,0 +1,118 @@
+mod events;
+mod game;
+mod helper;
+
+extern crate dotenv;
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+
+use crate::game::GameExecutionState;
+use game::Game;
+use lambda_runtime::{error::HandlerError, lambda, Context};
+use reqwest::blocking::Response;
+use simple_logger::SimpleLogger;
+use std::error::Error;
+use std::io::{BufRead, BufReader, Read};
+use std::env;
+
+const GAME_STREAM_ENDPOINT: &'static str = "https://lichess.org/api/bot/game/stream";
+
+#[derive(Deserialize, Clone)]
+struct PlayGameEvent {
+    #[serde(rename = "gameId")]
+    game_id: String,
+    #[serde(rename = "authToken")]
+    auth_token: String,
+    #[serde(rename = "botId")]
+    bot_id: String,
+    #[serde(rename = "expectedHalfMoves")]
+    expected_half_moves: u32,
+}
+
+#[derive(Serialize, Clone)]
+struct PlayGameOutput {}
+
+/// Entry point for the lambda function implementation
+fn main() -> Result<(), Box<dyn Error>> {
+    SimpleLogger::new().with_level(log::LevelFilter::Info).init()?;
+    lambda!(game_handler);
+    Ok(())
+}
+
+///// Entry point for standard rust app for testing
+//fn main() -> Result<(), Box<dyn Error>> {
+//    dotenv::dotenv().ok();
+//    SimpleLogger::new().with_level(log::LevelFilter::Info).init()?;
+//    uncontextualised_game_handler(PlayGameEvent {
+//        game_id: env::var("GAME_ID")?,
+//        auth_token: env::var("AUTH_TOKEN")?,
+//        bot_id: env::var("BOT_ID")?,
+//        expected_half_moves: env::var("EXPECTED_HALF_MOVES")?.parse()?
+//    })
+//        .map(|_| ())
+//        .map_err(|err| Box::new(err) as Box<dyn Error>)
+//}
+
+fn game_handler(e: PlayGameEvent, _ctx: Context) -> Result<PlayGameOutput, HandlerError> {
+    uncontextualised_game_handler(e)
+}
+
+fn uncontextualised_game_handler(e: PlayGameEvent) -> Result<PlayGameOutput, HandlerError> {
+    log::info!("Initializing game loop");
+    let mut game = Game::new(
+        e.game_id.to_owned(),
+        e.bot_id.to_owned(),
+        e.expected_half_moves,
+        e.auth_token.as_str(),
+    );
+
+    log::info!("Attempting to open stream for game {}", e.game_id);
+    let mut reader = open_game_stream(&e.game_id, &e.auth_token)?;
+    while let read_result = readline(&mut reader)? {
+        match read_result {
+            ReadResult::End => break,
+            ReadResult::Line(s) => {
+                if !s.is_empty() {
+                    log::info!("Received event: {}", s);
+                    match game
+                        .process_event(s.as_str())
+                        .map_err(|err| HandlerError::from(err.as_str()))?
+                    {
+                        GameExecutionState::Running => continue,
+                        GameExecutionState::Finished => break,
+                    }
+                }
+            }
+        }
+    }
+    Ok(PlayGameOutput {})
+}
+
+fn open_game_stream(
+    game_id: &String,
+    auth_token: &String,
+) -> Result<BufReader<Response>, HandlerError> {
+    reqwest::blocking::Client::new()
+        .get(format!("{}/{}", GAME_STREAM_ENDPOINT, game_id).as_str())
+        .bearer_auth(auth_token)
+        .send()
+        .map(|response| BufReader::new(response))
+        .map_err(|err| HandlerError::from(format!("{}", err).as_str()))
+}
+
+enum ReadResult {
+    Line(String),
+    End,
+}
+
+fn readline<R: Read>(bufreader: &mut BufReader<R>) -> Result<ReadResult, HandlerError> {
+    let mut dest = String::new();
+    match bufreader.read_line(&mut dest) {
+        Ok(0) => Ok(ReadResult::End),
+        Ok(_) => Ok(ReadResult::Line(String::from(dest.trim()))),
+        Err(error) => Err(HandlerError::from(format!("{}", error).as_str())),
+    }
+}
