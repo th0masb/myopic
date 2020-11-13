44af8aff5108ae10d848627b7c0a6fc34acd0eab
Thomas Ball <tomoliverball@gmail.com>
2020-11-13 16:28:50 +0000

    [MYO-16] Outlining basic impl structure

---
+++ game-lambda/src/events.rs
@@ -0,0 +1,149 @@
+use serde_derive;
+use serde;
+
+#[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
+#[serde(tag = "type")]
+pub enum GameEvent {
+    #[serde(rename = "gameFull")]
+    GameFull {
+        id: String,
+        white: Player,
+        black: Player,
+        state: GameState,
+    },
+
+    #[serde(rename = "gameState")]
+    State {
+        #[serde(flatten)]
+        state: GameState
+    }
+}
+
+#[derive(Deserialize, Debug, Clone, Eq, PartialEq)]
+pub struct GameState {
+    pub moves: String,
+    pub wtime: u64,
+    pub btime: u64,
+    pub winc: u64,
+    pub binc: u64,
+    pub wdraw: bool,
+    pub bdraw: bool,
+    pub status: String,
+}
+
+#[derive(Deserialize, Debug, Clone, Eq, PartialEq)]
+pub struct Player {
+    pub id: String,
+    pub name: String,
+    pub title: Option<String>,
+    pub rating: usize,
+    pub provisional: bool,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use serde_json;
+
+    #[test]
+    fn deserialize_state() {
+        let json = r#"{
+            "type": "gameState",
+            "moves": "e2e4 c7c5",
+            "wtime": 1000,
+            "btime": 1000,
+            "winc": 0,
+            "binc": 0,
+            "wdraw": false,
+            "bdraw": false,
+            "status": "started",
+            "other": "value"
+        }"#;
+
+        match serde_json::from_str::<GameEvent>(json) {
+            Err(error) => panic!(format!("Parse error {:?}", error)),
+            Ok(event) => match event {
+                GameEvent::GameFull { .. }  => panic!(format!("Wrong event {:?}", event)),
+                GameEvent::State { state } => assert_eq!(
+                    GameState {
+                        moves: String::from("e2e4 c7c5"),
+                        wtime: 1000,
+                        btime: 1000,
+                        winc: 0,
+                        binc: 0,
+                        wdraw: false,
+                        bdraw: false,
+                        status: String::from("started")
+                    }, state)
+            }
+        }
+    }
+
+    #[test]
+    fn deserialize_game_full() {
+        let json = r#"{
+            "type": "gameFull",
+            "id": "123",
+            "other": "value",
+            "white": {
+                "id": "th0masb",
+                "name": "th0masb",
+                "title": null,
+                "rating": 1500,
+                "provisional": true,
+                "other": "value"
+            },
+            "black": {
+                "id": "myopic-bot",
+                "name": "myopic-bot",
+                "title": "BOT",
+                "rating": 1500,
+                "provisional": true
+            },
+            "state": {
+                "moves": "e2e4 e7e5",
+                "wtime": 1000,
+                "btime": 1000,
+                "winc": 0,
+                "binc": 0,
+                "wdraw": false,
+                "bdraw": false,
+                "status": "started"
+            }
+        }"#;
+
+        match serde_json::from_str::<GameEvent>(json) {
+            Err(error) => panic!(format!("Parse error {:?}", error)),
+            Ok(event) => match event {
+                GameEvent::State { .. } => panic!(format!("Wrong type {:?}", event)),
+                GameEvent::GameFull { id, white, black, state } => {
+                    assert_eq!("123", id);
+                    assert_eq!(Player {
+                        id: String::from("th0masb"),
+                        name: String::from("th0masb"),
+                        title: None,
+                        rating: 1500,
+                        provisional: true
+                    }, white);
+                    assert_eq!(Player {
+                        id: String::from("myopic-bot"),
+                        name: String::from("myopic-bot"),
+                        title: Some(String::from("BOT")),
+                        rating: 1500,
+                        provisional: true
+                    }, black);
+                    assert_eq!(GameState {
+                        moves: String::from("e2e4 e7e5"),
+                        wtime: 1000,
+                        btime: 1000,
+                        winc: 0,
+                        binc: 0,
+                        wdraw: false,
+                        bdraw: false,
+                        status: String::from("started")
+                    }, state);
+                }
+            }
+        }
+    }
+}
--- game-lambda/src/main.rs
+++ game-lambda/src/main.rs
@@ -1,3 +1,6 @@
+mod events;
+mod parse;
+
 extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
@@ -7,45 +10,14 @@ extern crate serde_derive;
 use std::error::Error;
 use reqwest::blocking::{Client, Request, Response};
 use std::io::{Read, BufReader, BufRead};
+use events::{GameEvent, GameState, Player};
+use myopic_core::Side;
 
-#[derive(Deserialize, Debug, Clone, PartialEq, Eq)]
-#[serde(tag = "type")]
-enum GameEvent {
-    #[serde(rename = "gameFull")]
-    GameFull {
-        id: String,
-        white: Player,
-        black: Player,
-        state: GameState,
-    },
-
-    #[serde(rename = "gameState")]
-    State {
-        #[serde(flatten)]
-        state: GameState
-    }
+struct Tracking {
+    side: Side
 }
 
-#[derive(Deserialize, Debug, Clone, Eq, PartialEq)]
-struct GameState {
-    moves: String,
-    wtime: u64,
-    btime: u64,
-    winc: u64,
-    binc: u64,
-    wdraw: bool,
-    bdraw: bool,
-    status: String,
-}
-
-#[derive(Deserialize, Debug, Clone, Eq, PartialEq)]
-struct Player {
-    id: String,
-    name: String,
-    title: Option<String>,
-    rating: usize,
-    provisional: bool,
-}
+const LAMBDA_ID: &'static str = "myopic-bot";
 
 fn main() {
     let client = reqwest::blocking::Client::new();
@@ -55,6 +27,7 @@ fn main() {
         .send()
         .unwrap();
 
+    let mut tracking = Tracking { side: Side::White };
     let mut reader = BufReader::new(response);
     while let readResult = readline(&mut reader) {
         match readResult {
@@ -63,16 +36,39 @@ fn main() {
             ReadResult::Line(s) => {
                 if !s.is_empty() {
                     match serde_json::from_str(s.as_str()) {
-                        Err(error) => println!("Error during parse: {:?}", error),
+                        Err(error) => Err(format!("Error during parse: {:?}", error)),
                         Ok(event) => match event {
-                            GameEvent::GameFull { .. } => println!("Parsed gameFull: {:?}", event),
-                            GameEvent::State { .. } => println!("Parsed state: {:?}", event),
+                            GameEvent::GameFull {
+                                id: _id, white, black, state
+                            } => process_game_full(white, black, state, &mut tracking),
+                            GameEvent::State {
+                                state
+                            } => process_game_state(tracking.side, state),
                         }
-                    }
+                    };
                 }
             },
         }
     }
+}
+
+fn process_game_full(
+    white: Player,
+    black: Player,
+    state: GameState,
+    tracking: &mut Tracking
+) -> Result<(), String> {
+    // Track the side this lambda function is playing as for future gamestates
+    match (white.id.as_str(), black.id.as_str()) {
+        (LAMBDA_ID, _) => tracking.side = Side::White,
+        (_, LAMBDA_ID) => tracking.side = Side::Black,
+        _              => return Err(format!("Unrecognized names")),
+    }
+    process_game_state(tracking.side, state)
+}
+
+fn process_game_state(lambda_side: Side, state: GameState) -> Result<(), String> {
+    return Ok(())
 }
 
 //fn main() {
@@ -111,113 +107,3 @@ fn readline<R: Read>(bufreader: &mut BufReader<R>) -> ReadResult {
         _ => ReadResult::Err,
     }
 }
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use serde_json;
-
-    #[test]
-    fn deserialize_state() {
-        let json = r#"{
-            "type": "gameState",
-            "moves": "e2e4 c7c5",
-            "wtime": 1000,
-            "btime": 1000,
-            "winc": 0,
-            "binc": 0,
-            "wdraw": false,
-            "bdraw": false,
-            "status": "started",
-            "other": "value"
-        }"#;
-
-        match serde_json::from_str::<GameEvent>(json) {
-            Err(error) => panic!(format!("Parse error {:?}", error)),
-            Ok(event) => match event {
-                GameEvent::GameFull { .. }  => panic!(format!("Wrong event {:?}", event)),
-                GameEvent::State { state } => assert_eq!(
-                    GameState {
-                        moves: String::from("e2e4 c7c5"),
-                        wtime: 1000,
-                        btime: 1000,
-                        winc: 0,
-                        binc: 0,
-                        wdraw: false,
-                        bdraw: false,
-                        status: String::from("started")
-                    }
-                , state)
-            }
-        }
-    }
-
-    #[test]
-    fn deserialize_game_full() {
-        let json = r#"{
-            "type": "gameFull",
-            "id": "123",
-            "other": "value",
-            "white": {
-                "id": "th0masb",
-                "name": "th0masb",
-                "title": null,
-                "rating": 1500,
-                "provisional": true,
-                "other": "value"
-            },
-            "black": {
-                "id": "myopic-bot",
-                "name": "myopic-bot",
-                "title": "BOT",
-                "rating": 1500,
-                "provisional": true
-            },
-            "state": {
-                "moves": "e2e4 e7e5",
-                "wtime": 1000,
-                "btime": 1000,
-                "winc": 0,
-                "binc": 0,
-                "wdraw": false,
-                "bdraw": false,
-                "status": "started"
-            }
-        }"#;
-
-        match serde_json::from_str::<GameEvent>(json) {
-            Err(error) => panic!(format!("Parse error {:?}", error)),
-            Ok(event) => match event {
-                GameEvent::State { .. } => panic!(format!("Wrong type {:?}", event)),
-                GameEvent::GameFull { id, white, black, state } => {
-                    assert_eq!("123", id);
-                    assert_eq!(Player {
-                        id: String::from("th0masb"),
-                        name: String::from("th0masb"),
-                        title: None,
-                        rating: 1500,
-                        provisional: true
-                    }, white);
-                    assert_eq!(Player {
-                        id: String::from("myopic-bot"),
-                        name: String::from("myopic-bot"),
-                        title: Some(String::from("BOT")),
-                        rating: 1500,
-                        provisional: true
-                    }, black);
-                    assert_eq!(GameState {
-                        moves: String::from("e2e4 e7e5"),
-                        wtime: 1000,
-                        btime: 1000,
-                        winc: 0,
-                        binc: 0,
-                        wdraw: false,
-                        bdraw: false,
-                        status: String::from("started")
-                    }, state);
-                }
-            }
-        }
-    }
-}
-
