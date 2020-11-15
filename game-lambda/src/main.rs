f442be29dbaac08844eac863ecb8111fcdc2a536
Thomas Ball <tomoliverball@gmail.com>
2020-11-15 17:30:04 +0000

    [MYO-16] Converted main method into lambda format

--- game-lambda/src/main.rs
+++ game-lambda/src/main.rs
@@ -8,36 +8,63 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
+use lambda_runtime::{error::HandlerError, lambda, Context};
 use std::io::{Read, BufReader, BufRead};
 use events::GameEvent;
 use helper::*;
 use game::Game;
+use std::error::Error;
+use simple_logger::SimpleLogger;
 
-const EXPECTED_HALF_MOVE_COUNT: u32 = 60;
-const LAMBDA_ID: &'static str = "myopic-bot";
+const GAME_STREAM_ENDPOINT: &'static str = "https://lichess.org/api/bot/game/stream";
 
+#[derive(Deserialize, Clone)]
+struct PlayGameEvent {
+    #[serde(rename = "gameId")]
+    game_id: String,
+    #[serde(rename = "authToken")]
+    auth_token: String,
+    #[serde(rename = "botId")]
+    bot_id: String,
+    #[serde(rename = "expectedHalfMoves")]
+    expected_half_moves: String,
+}
 
-fn main() {
-    let client = reqwest::blocking::Client::new();
-    let game_id = "xxx";
+#[derive(Serialize, Clone)]
+struct PlayGameOutput {
+}
 
-    let response = client.get("https://lichess.org/api/bot/game/stream/mtjmNyVe")
-        .bearer_auth("xxx")
+fn main() -> Result<(), Box<dyn Error>> {
+    SimpleLogger::new().with_level(log::LevelFilter::Info).init()?;
+    lambda!(game_handler);
+    Ok(())
+}
+
+fn game_handler(
+    e: PlayGameEvent,
+    _ctx: Context,
+) -> Result<PlayGameOutput, HandlerError> {
+
+    let game_stream = reqwest::blocking::Client::new()
+        .get(format!("{}/{}", GAME_STREAM_ENDPOINT, e.game_id).as_str())
+        .bearer_auth(e.auth_token)
         .send()
-        .unwrap();
+        .map_err(|err| HandlerError::from(format!("{}", err).as_str()))?;
 
     let mut game = Game::new(
-        game_id.to_owned(),
-        LAMBDA_ID.to_owned(),
-        EXPECTED_HALF_MOVE_COUNT
+        e.game_id.to_owned(),
+        e.bot_id.to_owned(),
+        e.expected_half_moves.parse::<u32>()?,
     );
-    let mut reader = BufReader::new(response);
+
+    let mut reader = BufReader::new(game_stream);
     while let read_result = readline(&mut reader) {
         match read_result {
             ReadResult::End => break,
             ReadResult::Err => continue,
             ReadResult::Line(s) => {
                 if !s.is_empty() {
+                    // Need to add chat event support
                     match serde_json::from_str(s.as_str()) {
                         Err(error) => Err(format!("Error during parse: {:?}", error)),
                         Ok(event) => match event {
@@ -52,7 +79,8 @@ fn main() {
                 }
             },
         }
-    }
+    };
+    Ok(PlayGameOutput {})
 }
 
 enum ReadResult {
