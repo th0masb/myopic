1fd01274ed55b1184e1f8e5393294e45c6e1e60e
Thomas Ball <tomoliverball@gmail.com>
2020-11-19 18:04:33 +0000

    [MYO-16] Squash me

--- game-lambda/tools/build-release-binary.sh
+++ game-lambda/tools/build-release-binary.sh
@@ -1,23 +1,23 @@
-# Arg 1 => REPO_ADDRESS
-# Arg 2 => REPO_BRANCH
-# Arg 3 => PROJECT_MANIFEST_DIR
-# Arg 4 => PROJECT_BINARY_NAME
-# Arg 5 => Docker image name
+# Arg 1 => Output directory for the compiled binary
+# Arg 2 => REPO_ADDRESS
+# Arg 3 => REPO_BRANCH
+# Arg 4 => PROJECT_MANIFEST_DIR
+# Arg 5 => PROJECT_BINARY_NAME
+# Arg 6 => Docker image name
 function build_al2_runtime_lambda {
-  # Ensure the target directory exists
-  TARGET_DIR="$(pwd)/$(dirname "$0")/../target/amazonlinux2"
-  mkdir -p "$TARGET_DIR"
+  # Make sure the output directory exists
+  mkdir -p "$1"
 
   # Build the release binary within the al2 container forwarding
   # the parameters to the internal implementation script via
   # environment variables
   docker run \
-    --env REPO_ADDRESS="$1" \
-    --env REPO_BRANCH="$2" \
-    --env PROJECT_MANIFEST_DIR="$3" \
-    --env PROJECT_BINARY_NAME="$4" \
-    "$5" /bin/bash -c "/root/build-release-binary-internal.sh"
+    --env REPO_ADDRESS="$2" \
+    --env REPO_BRANCH="$3" \
+    --env PROJECT_MANIFEST_DIR="$4" \
+    --env PROJECT_BINARY_NAME="$5" \
+    "$6" /bin/bash -c "/root/build-release-binary-internal.sh"
 
   # Lift the compiled binary out of the container
-  docker cp "$(docker ps -alq):/root/$4" "$TARGET_DIR"
+  docker cp "$(docker ps -alq):/root/$5" "$1"
 }
