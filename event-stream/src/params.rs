ba7204a166a253a7913b3b44604d7ed98fe13328
Thomas Ball <tomoliverball@gmail.com>
2020-12-08 15:28:51 +0000

    [ISSUE-36] Updated event-stream to new lambda payload

--- event-stream/src/params.rs
+++ event-stream/src/params.rs
@@ -2,11 +2,19 @@ use std::env;
 use std::error::Error;
 
 /// Keys for required environment variables
-const MYOPIC_AUTH_TOKEN: &'static str = "MYOPIC_AUTH_TOKEN";
-const MYOPIC_REGION: &'static str = "MYOPIC_REGION";
-const MYOPIC_FUNCTION_NAME: &'static str = "MYOPIC_FUNCTION_NAME";
-const MYOPIC_BOT_ID: &'static str = "MYOPIC_BOT_ID";
 const MYOPIC_EXPECTED_HALF_MOVES: &'static str = "MYOPIC_EXPECTED_HALF_MOVES";
+// Lambda variables
+const MYOPIC_FUNCTION_REGION: &'static str = "MYOPIC_FUNCTION_REGION";
+const MYOPIC_FUNCTION_NAME: &'static str = "MYOPIC_FUNCTION_NAME";
+// Lichess variables
+const MYOPIC_LICHESS_AUTH_TOKEN: &'static str = "MYOPIC_LICHESS_AUTH_TOKEN";
+const MYOPIC_LICHESS_BOT_ID: &'static str = "MYOPIC_LICHESS_BOT_ID";
+// Opening table variables
+const MYOPIC_OPENING_TABLE_NAME: &'static str = "MYOPIC_OPENING_TABLE_NAME";
+const MYOPIC_OPENING_TABLE_REGION: &'static str = "MYOPIC_OPENING_TABLE_REGION";
+const MYOPIC_OPENING_TABLE_POSITION_KEY: &'static str = "MYOPIC_OPENING_TABLE_POSITION_KEY";
+const MYOPIC_OPENING_TABLE_MOVE_KEY: &'static str = "MYOPIC_OPENING_TABLE_MOVE_KEY";
+// Timing variables
 const MYOPIC_MIN_INITIAL_TIME_SECS: &'static str = "MYOPIC_MIN_INITIAL_TIME_SECS";
 const MYOPIC_MAX_INITIAL_TIME_SECS: &'static str = "MYOPIC_MAX_INITIAL_TIME_SECS";
 const MYOPIC_MIN_INCREMENT_SECS: &'static str = "MYOPIC_MIN_INCREMENT_SECS";
@@ -16,11 +24,15 @@ const MYOPIC_INCREMENT_ALLOWANCE_MINS: &'static str = "MYOPIC_INCREMENT_ALLOWANC
 
 #[derive(Debug, Clone)]
 pub struct ApplicationParameters {
-    pub auth_token: String,
-    pub region: String,
-    pub bot_id: String,
-    pub function_name: String,
     pub expected_half_moves: u32,
+    pub function_region: String,
+    pub function_name: String,
+    pub lichess_auth_token: String,
+    pub lichess_bot_id: String,
+    pub opening_table_name: String,
+    pub opening_table_region: String,
+    pub opening_table_position_key: String,
+    pub opening_table_move_key: String,
     pub min_initial_time_secs: u32,
     pub max_initial_time_secs: u32,
     pub min_increment_secs: u32,
@@ -32,11 +44,15 @@ pub struct ApplicationParameters {
 impl ApplicationParameters {
     pub fn load() -> Result<ApplicationParameters, Box<dyn Error>> {
         Ok(ApplicationParameters {
-            auth_token: env::var(MYOPIC_AUTH_TOKEN)?,
             expected_half_moves: env::var(MYOPIC_EXPECTED_HALF_MOVES)?.parse()?,
-            bot_id: env::var(MYOPIC_BOT_ID)?,
+            function_region: env::var(MYOPIC_FUNCTION_REGION)?,
             function_name: env::var(MYOPIC_FUNCTION_NAME)?,
-            region: env::var(MYOPIC_REGION)?,
+            lichess_auth_token: env::var(MYOPIC_LICHESS_AUTH_TOKEN)?,
+            lichess_bot_id: env::var(MYOPIC_LICHESS_BOT_ID)?,
+            opening_table_name: env::var(MYOPIC_OPENING_TABLE_NAME)?,
+            opening_table_region: env::var(MYOPIC_OPENING_TABLE_REGION)?,
+            opening_table_position_key: env::var(MYOPIC_OPENING_TABLE_POSITION_KEY)?,
+            opening_table_move_key: env::var(MYOPIC_OPENING_TABLE_MOVE_KEY)?,
             min_initial_time_secs: env::var(MYOPIC_MIN_INITIAL_TIME_SECS)?.parse()?,
             max_initial_time_secs: env::var(MYOPIC_MAX_INITIAL_TIME_SECS)?.parse()?,
             min_increment_secs: env::var(MYOPIC_MIN_INCREMENT_SECS)?.parse()?,
@@ -52,13 +68,17 @@ impl ApplicationParameters {
         depth: u8,
     ) -> Result<String, Box<dyn Error>> {
         serde_json::to_string(&PlayGameEvent {
-            depth,
-            game_id,
-            region: self.region.clone(),
-            auth_token: self.auth_token.clone(),
-            function_name: self.function_name.clone(),
-            bot_id: self.bot_id.clone(),
             expected_half_moves: self.expected_half_moves,
+            function_depth_remaining: depth,
+            function_region: self.function_region.clone(),
+            function_name: self.function_name.clone(),
+            lichess_game_id: game_id,
+            lichess_auth_token: self.lichess_auth_token.clone(),
+            lichess_bot_id: self.lichess_bot_id.clone(),
+            opening_table_name: self.opening_table_name.clone(),
+            opening_table_region: self.opening_table_region.clone(),
+            opening_table_position_key: self.opening_table_position_key.clone(),
+            opening_table_move_key: self.opening_table_move_key.clone(),
         })
         .map_err(|error| Box::new(error) as Box<dyn Error>)
     }
@@ -67,22 +87,36 @@ impl ApplicationParameters {
 /// The input payload of this lambda
 #[derive(Serialize, Deserialize, Clone)]
 struct PlayGameEvent {
-    /// The current call depth of the lambda invocation
-    depth: u8,
+    /// The current call depth of the lambda invokation
+    #[serde(rename = "functionDepthRemaining")]
+    function_depth_remaining: u8,
     /// The region this lambda is deployed in
-    region: String,
+    #[serde(rename = "functionRegion")]
+    function_region: String,
     /// The name of this lambda function
     #[serde(rename = "functionName")]
     function_name: String,
     /// The lichess game id this lambda will participate in
-    #[serde(rename = "gameId")]
-    game_id: String,
+    #[serde(rename = "lichessGameId")]
+    lichess_game_id: String,
     /// An auth token for the lichess bot this lambda will play as
-    #[serde(rename = "authToken")]
-    auth_token: String,
+    #[serde(rename = "lichessAuthToken")]
+    lichess_auth_token: String,
     /// The id of the lichess bot this lambda will play as
-    #[serde(rename = "botId")]
-    bot_id: String,
+    #[serde(rename = "lichessBotId")]
+    lichess_bot_id: String,
+    /// The name of the dynamodb table used to store opening positions
+    #[serde(rename = "openingTableName")]
+    opening_table_name: String,
+    /// The region in which the opening table is deployed
+    #[serde(rename = "openingTableRegion")]
+    opening_table_region: String,
+    /// The name of the position key used as a pk in the opening table
+    #[serde(rename = "openingTablePositionKey")]
+    opening_table_position_key: String,
+    /// The name of the move key used in the opening table
+    #[serde(rename = "openingTableMoveKey")]
+    opening_table_move_key: String,
     /// How many half moves we expect the game to last for
     #[serde(rename = "expectedHalfMoves")]
     expected_half_moves: u32,
