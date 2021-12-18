99bc8c152256047909afb7d38788d7fed5b068ae
Thomas Ball <tomoliverball@gmail.com>
2021-12-18 11:55:48 +0000

    [133] Added new stack implementation for containerised lambda deployment

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -1,50 +1,72 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { MyopicLambdaStack } from '../lib/myopic-lambda-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { MyopicLambdaStack } from "../lib/myopic-lambda-stack";
 import { MyopicDatabaseStack } from "../lib/opening-db-stack";
-import { TestStack } from "../lib/test-stack";
+import { BotStack, LambdaConfigType } from "../lib/bot-stack";
 
-require('dotenv').config()
+require("dotenv").config();
 
 const app = new cdk.App();
 const envConfig = {
   region: process.env.MYOPIC_AWS_REGION!,
-  account: process.env.MYOPIC_AWS_ACCOUNT!
-}
+  account: process.env.MYOPIC_AWS_ACCOUNT!,
+};
 
-new TestStack(app, "MyopicTestStack", {
-  env: envConfig
-})
-
-new MyopicDatabaseStack(app, 'MyopicDatabaseStack', {
+new MyopicDatabaseStack(app, "MyopicDatabaseStack", {
   openingsTableName: process.env.OPENINGS_TABLE_NAME!,
   positionAttributeName: process.env.POSITION_ATTRIBUTE_NAME!,
   readCapacity: Number.parseInt(process.env.READ_CAPACITY!),
   writeCapacity: Number.parseInt(process.env.WRITE_CAPACITY!),
-  env: envConfig
+  env: envConfig,
 });
 
-new MyopicLambdaStack(app, 'MyopicLambdaStack', {
+new MyopicLambdaStack(app, "MyopicLambdaStack", {
   openingsTableName: process.env.OPENINGS_TABLE_NAME!,
   env: envConfig,
   gameLambdaConfig: {
     assetName: process.env.GAME_HANDLER_ASSET_NAME!,
     functionName: process.env.GAME_HANDLER_FUNCTION_NAME!,
-    timeout: cdk.Duration.minutes(Number.parseInt(process.env.GAME_HANDLER_FUNCTION_TIMEOUT_MINS!)),
+    timeout: cdk.Duration.minutes(
+      Number.parseInt(process.env.GAME_HANDLER_FUNCTION_TIMEOUT_MINS!)
+    ),
     memoryLimit: Number.parseInt(process.env.GAME_HANDLER_MEMORY_SIZE!),
   },
   benchLambdaConfig: {
     assetName: process.env.BENCHMARK_ASSET_NAME!,
     functionName: process.env.BENCHMARK_FUNCTION_NAME!,
-    timeout: cdk.Duration.minutes(Number.parseInt(process.env.BENCHMARK_FUNCTION_TIMEOUT_MINS!)),
+    timeout: cdk.Duration.minutes(
+      Number.parseInt(process.env.BENCHMARK_FUNCTION_TIMEOUT_MINS!)
+    ),
     memoryLimit: Number.parseInt(process.env.BENCHMARK_MEMORY_SIZE!),
   },
   moveLambdaConfig: {
     assetName: process.env.MOVE_ASSET_NAME!,
     functionName: process.env.MOVE_FUNCTION_NAME!,
-    timeout: cdk.Duration.minutes(Number.parseInt(process.env.MOVE_FUNCTION_TIMEOUT_MINS!)),
+    timeout: cdk.Duration.minutes(
+      Number.parseInt(process.env.MOVE_FUNCTION_TIMEOUT_MINS!)
+    ),
     memoryLimit: Number.parseInt(process.env.MOVE_MEMORY_SIZE!),
-  }
+  },
 });
 
+new BotStack(app, "Hyperopic", {
+  env: envConfig,
+  openingTableName: process.env.OPENINGS_TABLE_NAME!,
+  params: new Map([
+    [
+      LambdaConfigType.Move,
+      {
+        memory: 1792,
+        timeout: cdk.Duration.minutes(10),
+      },
+    ],
+    [
+      LambdaConfigType.Game,
+      {
+        memory: 128,
+        timeout: cdk.Duration.minutes(10),
+      },
+    ],
+  ]),
+});
