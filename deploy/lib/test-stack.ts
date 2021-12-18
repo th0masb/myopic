ad9b075ca1a3b2aaa3a57d61100824dab8c0e0cc
Thomas Ball <tomoliverball@gmail.com>
2021-12-18 11:55:48 +0000

    [133] Added new stack implementation for containerised lambda deployment

--- deploy/lib/test-stack.ts
+++
@@ -1,8 +0,0 @@
-import * as cdk from '@aws-cdk/core';
-import { DockerImageAsset } from '@aws-cdk/aws-ecr-assets';
-
-export class TestStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
-    super(scope, id, props);
-  }
-}
