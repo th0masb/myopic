fa0a6712bac7a9515fcb9dae04edc9417438a066
Thomas Ball <tomoliverball@gmail.com>
2021-12-29 00:14:55 +0000

    [146] Deleted old lambda deployment

--- deploy/bin/deploy.ts
+++ deploy/bin/deploy.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
-import { MyopicLambdaStack } from "../lib/myopic-lambda-stack";
 import { MyopicDatabaseStack } from "../lib/opening-db-stack";
 import { BotStack, LambdaConfigType } from "../lib/bot-stack";
 
@@ -19,35 +18,6 @@ new MyopicDatabaseStack(app, "MyopicDatabaseStack", {
   readCapacity: Number.parseInt(process.env.READ_CAPACITY!),
   writeCapacity: Number.parseInt(process.env.WRITE_CAPACITY!),
   env: envConfig,
-});
-
-new MyopicLambdaStack(app, "MyopicLambdaStack", {
-  openingsTableName: process.env.OPENINGS_TABLE_NAME!,
-  env: envConfig,
-  gameLambdaConfig: {
-    assetName: process.env.GAME_HANDLER_ASSET_NAME!,
-    functionName: process.env.GAME_HANDLER_FUNCTION_NAME!,
-    timeout: cdk.Duration.minutes(
-      Number.parseInt(process.env.GAME_HANDLER_FUNCTION_TIMEOUT_MINS!)
-    ),
-    memoryLimit: Number.parseInt(process.env.GAME_HANDLER_MEMORY_SIZE!),
-  },
-  benchLambdaConfig: {
-    assetName: process.env.BENCHMARK_ASSET_NAME!,
-    functionName: process.env.BENCHMARK_FUNCTION_NAME!,
-    timeout: cdk.Duration.minutes(
-      Number.parseInt(process.env.BENCHMARK_FUNCTION_TIMEOUT_MINS!)
-    ),
-    memoryLimit: Number.parseInt(process.env.BENCHMARK_MEMORY_SIZE!),
-  },
-  moveLambdaConfig: {
-    assetName: process.env.MOVE_ASSET_NAME!,
-    functionName: process.env.MOVE_FUNCTION_NAME!,
-    timeout: cdk.Duration.minutes(
-      Number.parseInt(process.env.MOVE_FUNCTION_TIMEOUT_MINS!)
-    ),
-    memoryLimit: Number.parseInt(process.env.MOVE_MEMORY_SIZE!),
-  },
 });
 
 new BotStack(app, "Myopic", {
