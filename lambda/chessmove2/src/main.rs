5cd05ad1267a5f890d5673847e2c79a91415da82
Thomas Ball <tomoliverball@gmail.com>
2022-01-08 14:30:28 +0000

    [149] Completed impl of opening lookup service

--- lambda/chessmove2/src/main.rs
+++ lambda/chessmove2/src/main.rs
@@ -1,11 +1,16 @@
-mod lookup;
+mod openings;
 
-use crate::lookup::{DynamoOpeningMoveService, LookupMoveService};
+use crate::openings::DynamoOpeningMoveService;
 use async_trait::async_trait;
 use lambda_payloads::chessmove2::*;
 use lambda_runtime::{handler_fn, Context, Error};
 use myopic_brain::{anyhow, Board, ChessBoard, Move};
 use simple_logger::SimpleLogger;
+
+#[async_trait]
+pub trait LookupMoveService<B: ChessBoard + Send> {
+    async fn lookup(&self, position: B) -> anyhow::Result<Option<Move>>;
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -20,15 +25,26 @@ async fn main() -> Result<(), Error> {
 async fn func(event: ChooseMoveEvent, _: Context) -> Result<ChooseMoveOutput, Error> {
     let mut board = Board::default();
     board.play_uci(event.moves_played.as_str())?;
-    let lookups: Vec<Box<dyn LookupMoveService<Board>>> = vec![
-        Box::new(DynamoOpeningMoveService::from(event.opening_table.clone()))
-    ];
-    for service in lookups.iter() {
-        service.lookup(board.clone()).await?;
+
+    // Attempt to lookup precomputed moves from various sources
+    let lookup_services: Vec<Box<dyn LookupMoveService<Board>>> = vec![Box::new(
+        DynamoOpeningMoveService::try_from(event.opening_table.clone())?,
+    )];
+    for service in lookup_services.iter() {
+        match service.lookup(board.clone()).await? {
+            None => {}
+            Some(mv) => {
+                return Ok(ChooseMoveOutput {
+                    best_move: mv.uci_format(),
+                    // TODO Alter return payload to support this being optional
+                    depth_searched: 0,
+                    search_duration_millis: 0,
+                    eval: 0,
+                });
+            }
+        }
     }
 
+    // Otherwise we need to perform a search
     todo!()
-    //    let first_name = event["firstName"].as_str().unwrap_or("world");
-    //
-    //    Ok(json!({ "message": format!("Hello, {}!", first_name) }))
 }
