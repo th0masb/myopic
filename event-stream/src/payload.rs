a3d499f9a234cf0ed8e6e8be3f6d0f81643cb1b9
Thomas Ball <tomoliverball@gmail.com>
2022-05-01 17:38:24 +0100

    [149] Update chessgame payload, add hyperopic to docker-compose.yaml

--- event-stream/src/payload.rs
+++ event-stream/src/payload.rs
@@ -7,15 +7,6 @@ use serde_derive::{Serialize, Deserialize};
 /// The input payload of this lambda
 #[derive(Serialize, Deserialize, Clone)]
 pub struct PlayGameEvent {
-    /// The current call depth of the lambda invocation
-    #[serde(rename = "functionDepthRemaining")]
-    pub function_depth_remaining: u8,
-    /// The name of this lambda function
-    #[serde(rename = "functionName")]
-    pub function_name: String,
-    /// The region this lambda is deployed in
-    #[serde(rename = "functionRegion")]
-    pub function_region: String,
     /// The name of the lambda function move searching is delegated to
     #[serde(rename = "moveFunctionName")]
     pub move_function_name: String,
@@ -31,18 +22,6 @@ pub struct PlayGameEvent {
     /// The id of the lichess bot this lambda will play as
     #[serde(rename = "lichessBotId")]
     pub lichess_bot_id: String,
-    /// The name of the dynamodb table used to store opening positions
-    #[serde(rename = "openingTableName")]
-    pub opening_table_name: String,
-    /// The region in which the opening table is deployed
-    #[serde(rename = "openingTableRegion")]
-    pub opening_table_region: String,
-    /// The name of the position key used as a pk in the opening table
-    #[serde(rename = "openingTablePositionKey")]
-    pub opening_table_position_key: String,
-    /// The name of the move key used in the opening table
-    #[serde(rename = "openingTableMoveKey")]
-    pub opening_table_move_key: String,
     /// How many seconds to wait for the first full move to take place
     /// before aborting the game
     #[serde(rename = "abortAfterSecs")]
