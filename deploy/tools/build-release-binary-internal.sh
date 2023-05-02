1866d48553d24b1628a8953dac75e276c9286c4a
Thomas Ball <tomoliverball@gmail.com>
2023-05-02 19:10:40 +0100

    Delete redundant deploy/tools dir

--- deploy/tools/build-release-binary-internal.sh
+++
@@ -1,5 +0,0 @@
-cd ~
-git clone "$REPO_ADDRESS" && cd "$(basename "$_" .git)"
-git checkout "$REPO_BRANCH"
-cargo build --manifest-path "$PROJECT_MANIFEST_DIR/Cargo.toml" --release
-cp -f "$PROJECT_MANIFEST_DIR/target/release/$PROJECT_BINARY_NAME" ~
