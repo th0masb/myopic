83a46ae5aa33c918eea099191271a160ff9c99c8
Thomas Ball <tomoliverball@gmail.com>
2023-05-26 18:57:38 +0100

    Change the Cluster stack to use public subnet so we don't need a nat gateway

--- deploy/lib/ClusterStack.ts
+++ deploy/lib/ClusterStack.ts
@@ -1,9 +1,7 @@
-import {aws_ec2 as ec2, aws_ecs as ecs, aws_iam as iam, Stack} from "aws-cdk-lib";
+import {aws_ec2 as ec2, aws_ecs as ecs, Stack} from "aws-cdk-lib";
 import {Construct} from "constructs";
-import {InstanceClass, InstanceSize, InstanceType} from "aws-cdk-lib/aws-ec2";
-import {DockerImageAsset} from "aws-cdk-lib/aws-ecr-assets";
-import * as path from "path";
-import {AccountAndRegion, EventStreamConfig} from "../config";
+import {InstanceClass, InstanceSize, InstanceType, SubnetType} from "aws-cdk-lib/aws-ec2";
+import {AccountAndRegion} from "../config";
 
 export class ClusterStack extends Stack {
     readonly cluster: ecs.Cluster
@@ -17,62 +15,22 @@ export class ClusterStack extends Stack {
         this.cluster = new ecs.Cluster(this, "ClusterNodes", {
             clusterName: "Myopic",
             vpc: new ec2.Vpc(this, "Vpc", {
-                ipAddresses: ec2.IpAddresses.cidr("10.1.1.0/24"),
-                maxAzs: 1
+                ipAddresses: ec2.IpAddresses.cidr("10.2.1.0/24"),
+                maxAzs: 1,
+                // Expensive!
+                natGateways: 0,
+                subnetConfiguration: [
+                    {
+                        name: "Public",
+                        subnetType: SubnetType.PUBLIC
+                    }
+                ]
             }),
-
             capacity: {
                 instanceType: InstanceType.of(InstanceClass.T3A, InstanceSize.NANO),
                 maxCapacity: 2,
                 allowAllOutbound: true,
             }
         })
-        //const taskDefinition = new ecs.TaskDefinition(this, "EventStreamTaskDefinition", {
-        //    compatibility: ecs.Compatibility.EC2,
-        //})
-        //taskDefinition.addToTaskRolePolicy(this.createLambdaInvokePolicy(gameLambdaArn))
-        //const eventStreamImage = this.eventStreamImage()
-        //for (const bot of eventStreamConfig) {
-        //    taskDefinition.addContainer(bot.name, {
-        //        image: eventStreamImage,
-        //        memoryLimitMiB: 210,
-        //        environment: {
-        //            LICHESS_AUTH_TOKEN: process.env[bot.authTokenVar]!,
-        //            APP_CONFIG: JSON.stringify(bot.config)
-        //        },
-        //        logging: ecs.LogDrivers.awsLogs({
-        //            streamPrefix: "EventStream",
-        //            mode: ecs.AwsLogDriverMode.NON_BLOCKING,
-        //        })
-        //    })
-        //}
-        //new ecs.Ec2Service(this, "EventStreamService", {
-        //    cluster,
-        //    taskDefinition,
-        //    circuitBreaker: { rollback: true },
-        //    desiredCount: 1,
-        //    minHealthyPercent: 0,
-        //    maxHealthyPercent: 100,
-        //})
-    }
-
-    private createLambdaInvokePolicy(functionArn: string) {
-        const ps = new iam.PolicyStatement();
-        ps.addActions("lambda:InvokeFunction");
-        ps.addResources(functionArn)
-        return ps
-    }
-
-    private eventStreamImage() {
-        return ecs.ContainerImage.fromDockerImageAsset(
-            new DockerImageAsset(this, "EventStreamImage", {
-                directory: path.join(__dirname, "..", ".."),
-                file: path.join("tools", "workspace.dockerfile"),
-                buildArgs: {
-                    APP_DIR: "event-stream",
-                    APP_NAME: "event-stream",
-                },
-            })
-        )
     }
 }
