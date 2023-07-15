c487284dd0174a171fca8e79c8582c7fb203a228
Thomas Ball <tomoliverball@gmail.com>
2023-07-15 16:57:21 +0100

    Add pgn move parsing

--- hyperopic/src/parse.rs
+++ hyperopic/src/parse.rs
@@ -1,11 +1,14 @@
 use std::str::FromStr;
 
-use anyhow::{Error, Result};
+use anyhow::{anyhow, Error, Result};
 use lazy_static::lazy_static;
 use regex::Regex;
+use Move::{Enpassant, Normal, Null, Promote};
 
 use crate::board::iter;
-use crate::{hash, lift, piece_side, Board, Piece, PieceMap};
+use crate::{hash, lift, piece_side, Board, Piece, PieceMap, Square, Class, square_file, square_rank, piece_class};
+use crate::constants::class;
+use crate::moves::{Move, Moves};
 
 use crate::position::Position;
 
@@ -13,7 +16,14 @@ impl FromStr for Position {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        parse_fen(s)
+        parse_fen(s).or_else(|_| {
+            let mut position = Position::default();
+            for m in PGN_MOVE.find_iter(s) {
+                let m = parse_pgn_move(&position, m.as_str())?;
+                position.make(m)?
+            }
+            Ok(position)
+        })
     }
 }
 
@@ -21,14 +31,15 @@ pub struct StringIndexMap {
     content: Vec<String>,
 }
 
+const FILE_CHARS: [char; 8] = ['h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'];
+const RANK_CHARS: [char; 8] = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
 impl StringIndexMap {
     pub fn squares() -> StringIndexMap {
         StringIndexMap {
-            content: (1usize..=8)
+            content: RANK_CHARS
                 .into_iter()
-                .flat_map(|r| {
-                    ["h", "g", "f", "e", "d", "c", "b", "a"].map(|f| format!("{}{}", f, r))
-                })
+                .flat_map(|r| FILE_CHARS.iter().map(move |f| format!("{}{}", f, r)))
                 .collect(),
         }
     }
@@ -60,6 +71,12 @@ impl StringIndexMap {
                 .collect(),
         }
     }
+
+    pub fn pgn_pieces() -> StringIndexMap {
+        StringIndexMap {
+            content: vec!["", "N", "B", "R", "Q", "K"].into_iter().map(|s| s.to_owned()).collect(),
+        }
+    }
 }
 
 impl StringIndexMap {
@@ -73,14 +90,122 @@ impl StringIndexMap {
 }
 
 lazy_static! {
-    static ref FILE: Regex = r"([a-h])".parse().unwrap();
-    static ref RANK: Regex = r"([1-8])".parse().unwrap();
-    static ref SQUARE: Regex = r"([a-h][1-8])".parse().unwrap();
-    static ref SPACE: Regex = r"(\s+)".parse().unwrap();
-    static ref FEN_RANK: Regex = r"([pnbrqkPNBRQK1-8]{1,8})".parse().unwrap();
-    static ref TEST: Regex = format!("{}", SQUARE.as_str()).as_str().parse().unwrap();
+    // Index maps
     static ref SQUARE_MAP: StringIndexMap = StringIndexMap::squares();
     static ref FEN_PIECES_MAP: StringIndexMap = StringIndexMap::fen_pieces();
+
+    // Patterns
+    static ref SPACE: Regex = r"(\s+)".parse().unwrap();
+    static ref FEN_RANK: Regex = r"([pnbrqkPNBRQK1-8]{1,8})".parse().unwrap();
+
+    static ref FILE: Regex = r"([a-h])".parse().unwrap();
+    static ref RANK: Regex = r"([1-8])".parse().unwrap();
+    static ref INT: Regex = "([0-9]+)".parse().unwrap();
+    static ref SQUARE: Regex = r"([a-h][1-8])".parse().unwrap();
+
+    static ref PGN_PIECE: Regex = r"(N|B|R|Q|K)".parse().unwrap();
+    static ref PGN_CASTLE: Regex = r"(O-O(-O)?)".parse().unwrap();
+
+    static ref PGN_NORMAL_MOVE: Regex = format!(
+        r"({}?({}|{}|{})?x?{}(=[NBRQ])?[+#]?)",
+        PGN_PIECE.as_str(),
+        RANK.as_str(),
+        FILE.as_str(),
+        SQUARE.as_str(),
+        SQUARE.as_str(),
+    ).as_str().parse().unwrap();
+
+    static ref PGN_MOVE: Regex = format!(
+        "({}|{})",
+        PGN_NORMAL_MOVE.as_str(),
+        PGN_CASTLE.as_str()
+    ).as_str().parse().unwrap();
+}
+
+fn parse_pgn_move(position: &Position, input: &str) -> Result<Move> {
+    let moves = position.moves(&Moves::All);
+
+    if PGN_CASTLE.is_match(input) {
+        return moves.iter().find(|&m| {
+            if let Move::Castle { corner } = m {
+                *corner % 2 == if input == "O-O" { 0 } else { 1 }
+            } else {
+                false
+            }
+        }).cloned().ok_or(anyhow!("{} not legal", input))
+    }
+
+    let target = SQUARE
+        .find_iter(input)
+        .map(|m| SQUARE_MAP.get(m.as_str()))
+        .last()
+        .map(|mv| mv.clone());
+
+    let (move_piece_class, promote_piece_class) = parse_pgn_classes(input);
+    let move_piece_matches = |p: Class| move_piece_class == p;
+    let promote_piece_matches = |p: Class| promote_piece_class == p;
+    let move_matches_pawn = move_piece_matches(class::P);
+
+    let file = parse_extra_rank_file(&FILE, input);
+    let rank = parse_extra_rank_file(&RANK, input);
+    let matches_start = |sq: Square| matches_square(file, rank, sq);
+
+    moves.into_iter().filter(|m| {
+        match m {
+            Null | Move::Castle { .. } => false,
+            Enpassant { from, .. } => {
+                move_matches_pawn && target == position.enpassant && matches_start(*from)
+            },
+            Normal { moving, from, dest, .. } => {
+                move_piece_matches(piece_class(*moving))
+                    && target == Some(*dest) && matches_start(*from)
+            },
+            Promote { from, dest, promoted, .. } => {
+                move_matches_pawn &&
+                    target == Some(*dest) &&
+                    matches_start(*from) &&
+                    promote_piece_matches(piece_class(*promoted))
+            },
+        }
+    }).next().ok_or(anyhow!("No move matching {}", input))
+}
+
+fn matches_square(file: Option<char>, rank: Option<char>, square: Square) -> bool {
+    let sq_file = FILE_CHARS[square_file(square)];
+    let sq_rank = RANK_CHARS[square_rank(square)];
+    file.map(|f| f == sq_file).unwrap_or(true) &&
+        rank.map(|r| r == sq_rank).unwrap_or(true)
+}
+
+fn parse_extra_rank_file(re: &Regex, input: &str) -> Option<char> {
+    re.find_iter(input)
+        .map(|m| m.as_str().to_owned())
+        .collect::<Vec<_>>()
+        .into_iter()
+        .rev()
+        .skip(1)
+        .last()
+        .and_then(|s| s.chars().nth(0))
+}
+
+fn parse_pgn_classes(input: &str) -> (Class, Class) {
+    let matches: Vec<_> = PGN_PIECE.find_iter(input).map(|m| m.as_str().to_owned()).collect();
+    let is_promotion = input.contains("=");
+    let piece = matches.get(0).and_then(|s| s.chars().nth(0));
+    let (move_piece, promote_piece) = if is_promotion { (None, piece) } else { (piece, None) };
+    (parse_class(move_piece), parse_class(promote_piece))
+}
+
+fn parse_class(piece: Option<char>) -> Class {
+    match piece {
+        None => 0,
+        Some('N') => 1,
+        Some('B') => 2,
+        Some('R') => 3,
+        Some('Q') => 4,
+        Some('K') => 5,
+        _ => panic!(),
+    }
 }
 
 fn parse_fen(fen: &str) -> Result<Position> {
@@ -184,5 +309,144 @@ mod test_fen {
                 )
             )
         )
+    }
+}
+
+#[cfg(test)]
+mod test_single_move {
+    use crate::Board;
+    use std::str::FromStr;
+
+    use super::*;
+
+    fn execute_success_test(
+        expected: &'static str,
+        start_fen: &'static str,
+        pgn: &'static str,
+    ) -> Result<()> {
+        let mut board = start_fen.parse::<Position>()?;
+        let parsed_expected = Move::from_str(expected)?;
+        let pgn_parse = parse_pgn_move(&mut board, pgn)?;
+        assert_eq!(parsed_expected, pgn_parse);
+        Ok(())
+    }
+
+    #[test]
+    fn case_one() -> Result<()> {
+        execute_success_test(
+            "sbbg4f3wn",
+            "rn1qkbnr/pp2pppp/2p5/3p4/4P1b1/2N2N1P/PPPP1PP1/R1BQKB1R b KQkq - 0 4",
+            "Bxf3",
+        )
+    }
+
+    #[test]
+    fn case_two() -> Result<()> {
+        execute_success_test(
+            "ewe5f6f5",
+            "r2qkbnr/pp1np1pp/2p5/3pPp2/8/2N2Q1P/PPPP1PP1/R1B1KB1R w KQkq f6 0 7",
+            "exf6",
+        )
+    }
+
+    #[test]
+    fn case_three() -> Result<()> {
+        execute_success_test(
+            "pf7g8wnbn",
+            "r2q1bnr/pp1nkPpp/2p1p3/3p4/8/2N2Q1P/PPPP1PP1/R1B1KB1R w KQ - 1 9",
+            "fxg8=N",
+        )
+    }
+
+    #[test]
+    fn case_four() -> Result<()> {
+        execute_success_test(
+            "pf7g8wqbn",
+            "r2q1bnr/pp1nkPpp/2p1p3/3p4/8/2N2Q1P/PPPP1PP1/R1B1KB1R w KQ - 1 9",
+            "fxg8=Q",
+        )
+    }
+
+    #[test]
+    fn case_five() -> Result<()> {
+        execute_success_test(
+            "sbra8e8-",
+            "r5r1/ppqkb1pp/2p1pn2/3p2B1/3P4/2NB1Q1P/PPP2PP1/4RRK1 b - - 8 14",
+            "Rae8",
+        )
+    }
+
+    #[test]
+    fn case_six() -> Result<()> {
+        execute_success_test(
+            "swre1e2-",
+            "4rr2/ppqkb1p1/2p1p2p/3p4/3Pn2B/2NBRQ1P/PPP2PP1/4R1K1 w - - 2 18",
+            "R1e2",
+        )
+    }
+
+    #[test]
+    fn case_seven() -> Result<()> {
+        execute_success_test(
+            "sbrf3f6wb",
+            "5r2/ppqkb1p1/2p1pB1p/3p4/3Pn2P/2NBRr2/PPP1RPP1/6K1 b - - 0 20",
+            "R3xf6",
+        )
+    }
+
+    #[test]
+    fn case_eight() -> Result<()> {
+        execute_success_test(
+            "sbne4f2wp",
+            "5r2/ppqkb1p1/2p1pr1p/3p4/3Pn2P/2NBR3/PPP1RPP1/7K b - - 1 21",
+            "Nxf2+",
+        )
+    }
+
+    #[test]
+    fn case_nine() -> Result<()> {
+        execute_success_test(
+            "sbrf8f1wb",
+            "5r2/ppqkb1p1/2p1p2p/3p4/P2P3P/2N1R3/1PP3P1/5B1K b - - 0 24",
+            "Rf8xf1#",
+        )
+    }
+
+    #[test]
+    fn case_ten() -> Result<()> {
+        execute_success_test(
+            "cwk",
+            "r3k2r/pp1q1ppp/n1p2n2/4p3/3pP2P/3P1QP1/PPPN1PB1/R3K2R w KQkq - 1 13",
+            "O-O",
+        )
+    }
+
+    #[test]
+    fn case_eleven() -> Result<()> {
+        execute_success_test(
+            "cbq",
+            "r3k2r/pp1q1ppp/n1p2n2/4p3/3pP2P/3P1QP1/PPPN1PB1/R4RK1 b kq - 2 13",
+            "O-O-O",
+        )
+    }
+
+    #[test]
+    fn case_12() {
+        execute_success_test(
+            "pg7f8wqbr",
+            "rnbq1rk1/p4pPp/2pbp3/8/3P4/8/Pp2BPPP/R1BQK1NR w KQ - 0 12",
+            "gxf8=Q+",
+        )
+            .unwrap()
+    }
+
+    #[test]
+    fn case_13() {
+        execute_success_test(
+            "sbqd8f8wq",
+            "rnbq1Qk1/p4p1p/2pbp3/8/3P4/8/Pp2BPPP/R1BQK1NR b KQ - 0 12",
+            "Qxf8",
+        )
+            .unwrap()
     }
 }
