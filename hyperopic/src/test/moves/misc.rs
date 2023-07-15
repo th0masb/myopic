5f81265c9b31494997b17eeb3a1ddc6d82211088
Thomas Ball <tomoliverball@gmail.com>
2023-07-15 11:19:30 +0100

    Add promoting category to move tests

--- hyperopic/src/test/moves/misc.rs
+++ hyperopic/src/test/moves/misc.rs
@@ -4,7 +4,6 @@ use super::*;
 fn case_01() -> Result<()> {
     execute_test(TestCase {
         board: "rn1k3r/2q2ppp/2p3b1/1b2pP2/8/BPN2B2/2Q2PP1/R3K2R w KQkq e6 3 10",
-        #[rustfmt::skip]
         all: vec![
             "swra1a2-", "swra1b1-", "swra1c1-", "swra1d1-",
             "swba3b2-", "swba3c1-", "swba3b4-", "swba3c5-", "swba3d6-", "swba3e7-", "swba3f8-",
@@ -17,7 +16,6 @@ fn case_01() -> Result<()> {
             "swpg2g3-", "swpg2g4-",
             "swrh1g1-", "swrh1f1-", "swrh1h2-", "swrh1h3-", "swrh1h4-", "swrh1h5-", "swrh1h6-", "swrh1h7bp",
         ],
-        #[rustfmt::skip]
         attacks: vec![
             "swnc3b5bb",
             "swbf3c6bp",
@@ -25,7 +23,6 @@ fn case_01() -> Result<()> {
             "ewf5e6e5",
             "swrh1h7bp",
         ],
-        #[rustfmt::skip]
         attacks_checks: vec![
             "swnc3b5bb",
             "swbf3c6bp",
@@ -39,6 +36,8 @@ fn case_01() -> Result<()> {
             "swba3e7-",
             "cwq"
         ],
+        promotes: vec![
+        ]
     })
 }
 
@@ -46,7 +45,6 @@ fn case_01() -> Result<()> {
 fn case_02() -> Result<()> {
     execute_test(TestCase {
         board: "1r5r/P1k2ppp/2n3b1/b2p4/B2P4/2N5/p4PP1/1R2K2R w Kk - 5 20",
-        #[rustfmt::skip]
         all: vec![
             "swba4b3-", "swba4c2-", "swba4d1-", "swba4b5-", "swba4c6bn",
             "swrb1a1-", "swrb1c1-", "swrb1d1-", "swrb1b2-", "swrb1b3-", "swrb1b4-", "swrb1b5-", "swrb1b6-", "swrb1b7-", "swrb1b8br",
@@ -57,14 +55,12 @@ fn case_02() -> Result<()> {
             "swpg2g3-", "swpg2g4-",
             "swrh1g1-", "swrh1f1-", "swrh1h2-", "swrh1h3-", "swrh1h4-", "swrh1h5-", "swrh1h6-", "swrh1h7bp",
         ],
-        #[rustfmt::skip]
         attacks: vec![
             "swba4c6bn",
             "swrb1b8br",
             "swrh1h7bp",
             "pa7b8wnbr", "pa7b8wbbr", "pa7b8wrbr", "pa7b8wqbr",
         ],
-        #[rustfmt::skip]
         attacks_checks: vec![
             "swba4c6bn",
             "swrb1b8br", "swrb1b7-",
@@ -72,6 +68,10 @@ fn case_02() -> Result<()> {
             "pa7a8wn-",
             "pa7b8wnbr", "pa7b8wbbr", "pa7b8wrbr", "pa7b8wqbr",
         ],
+        promotes: vec![
+            "pa7a8wn-", "pa7a8wb-", "pa7a8wr-", "pa7a8wq-",
+            "pa7b8wnbr", "pa7b8wbbr", "pa7b8wrbr", "pa7b8wqbr",
+        ]
     })
 }
 
@@ -79,7 +79,6 @@ fn case_02() -> Result<()> {
 fn case_03() -> Result<()> {
     execute_test(TestCase {
         board: "r3k2r/2q2pp1/2p3b1/1b6/8/1PN2B2/2Q2PP1/R3RK2 w kq - 3 10",
-        #[rustfmt::skip]
         all: vec![
             "swkf1g1-",
             "swre1e2-",
@@ -87,10 +86,9 @@ fn case_03() -> Result<()> {
             "swnc3e2-", "swnc3b5bb",
             "swbf3e2-",
         ],
-        #[rustfmt::skip]
         attacks: vec!["swnc3b5bb"],
-        #[rustfmt::skip]
         attacks_checks: vec!["swnc3b5bb", "swre1e2-", "swqc2e2-"],
+        promotes: vec![]
     })
 }
 
@@ -116,14 +114,12 @@ fn case_03() -> Result<()> {
 fn case_06() -> Result<()> {
     execute_test(TestCase {
         board: "8/8/8/8/8/2k1R3/8/B6K b - - 3 10",
-        #[rustfmt::skip]
         all: vec![
             "sbkc3c2-", "sbkc3d2-", "sbkc3c4-", "sbkc3b4-",
         ],
-        #[rustfmt::skip]
         attacks: vec![],
-        #[rustfmt::skip]
         attacks_checks: vec![],
+        promotes: vec![],
     })
 }
 
@@ -131,20 +127,19 @@ fn case_06() -> Result<()> {
 fn case_07() -> Result<()> {
     execute_test(TestCase {
         board: "7k/8/8/3PpP2/8/8/8/7K w - e6 3 10",
-        #[rustfmt::skip]
         all: vec![
             "swpd5d6-", "swpf5f6-",
             "ewd5e6e5", "ewf5e6e5",
             "swkh1g1-", "swkh1g2-", "swkh1h2-",
         ],
-        #[rustfmt::skip]
         attacks: vec![
             "ewd5e6e5", "ewf5e6e5",
         ],
-        #[rustfmt::skip]
         attacks_checks: vec![
             "ewd5e6e5", "ewf5e6e5",
         ],
+        promotes: vec![
+        ]
     })
 }
 
@@ -152,21 +147,22 @@ fn case_07() -> Result<()> {
 fn case_08() -> Result<()> {
     execute_test(TestCase {
         board: "4k2b/1KP2rP1/8/2P5/8/8/8/8 w - - 3 10",
-        #[rustfmt::skip]
         all: vec![
             "swkb7b8-", "swkb7a8-", "swkb7a7-", "swkb7a6-", "swkb7b6-", "swkb7c6-", "swkb7c8-",
             "swpc5c6-",
             "pg7g8wn-", "pg7g8wb-", "pg7g8wr-", "pg7g8wq-",
             "pg7h8wnbb", "pg7h8wbbb", "pg7h8wrbb", "pg7h8wqbb",
         ],
-        #[rustfmt::skip]
         attacks: vec![
             "pg7h8wnbb", "pg7h8wbbb", "pg7h8wrbb", "pg7h8wqbb",
         ],
-        #[rustfmt::skip]
         attacks_checks: vec![
             "pg7g8wr-", "pg7g8wq-", "pg7h8wnbb", "pg7h8wbbb", "pg7h8wrbb", "pg7h8wqbb",
         ],
+        promotes: vec![
+            "pg7g8wn-", "pg7g8wb-", "pg7g8wr-", "pg7g8wq-",
+            "pg7h8wnbb", "pg7h8wbbb", "pg7h8wrbb", "pg7h8wqbb",
+        ]
     })
 }
 
@@ -174,14 +170,12 @@ fn case_08() -> Result<()> {
 fn case_10() -> Result<()> {
     execute_test(TestCase {
         board: "6k1/8/1K6/2Pp4/8/4b3/8/8 w - - 3 10",
-        #[rustfmt::skip]
         all: vec![
             "swkb6b5-", "swkb6c6-", "swkb6c7-", "swkb6b7-", "swkb6a7-", "swkb6a6-", "swkb6a5-",
         ],
-        #[rustfmt::skip]
         attacks: vec![],
-        #[rustfmt::skip]
         attacks_checks: vec![],
+        promotes: vec![]
     })
 }
 
@@ -189,12 +183,10 @@ fn case_10() -> Result<()> {
 fn case_11() -> Result<()> {
     execute_test(TestCase {
         board: "r1bk2br/p2pBpNp/n4n2/1p1NP2P/6P1/3P4/P1P1K3/q7 b - - 3 10",
-        #[rustfmt::skip]
         all: vec![],
-        #[rustfmt::skip]
         attacks: vec![],
-        #[rustfmt::skip]
         attacks_checks: vec![],
+        promotes: vec![]
     })
 }
 
@@ -202,18 +194,16 @@ fn case_11() -> Result<()> {
 fn case_12() -> Result<()> {
     execute_test(TestCase {
         board: "6rk/5p2/4pPp1/8/8/6P1/6PK/7R w - - 3 10",
-        #[rustfmt::skip]
         all: vec![
             "swkh2h3-", "swkh2g1-",
             "swpg3g4-",
             "swrh1g1-", "swrh1f1-", "swrh1e1-", "swrh1d1-", "swrh1c1-", "swrh1b1-", "swrh1a1-",
         ],
-        #[rustfmt::skip]
         attacks: vec![],
-        #[rustfmt::skip]
         attacks_checks: vec![
             "swkh2g1-"
         ],
+        promotes: vec![]
     })
 }
 
@@ -221,19 +211,17 @@ fn case_12() -> Result<()> {
 fn case_13() -> Result<()> {
     execute_test(TestCase {
         board: "4R3/1p1Q2rk/6p1/2p1BpP1/p1P1pP2/P7/1P6/K2q4 w - - 2 2",
-        #[rustfmt::skip]
         all: vec![
             "swqd7d1bq",
             "swka1a2-",
         ],
-        #[rustfmt::skip]
         attacks: vec![
             "swqd7d1bq",
         ],
-        #[rustfmt::skip]
         attacks_checks: vec![
             "swqd7d1bq",
         ],
+        promotes: vec![]
     })
 }
 
@@ -241,14 +229,12 @@ fn case_13() -> Result<()> {
 fn case_14() -> Result<()> {
     execute_test(TestCase {
         board: "8/8/3p4/2pP3R/2Pk1pPQ/3B4/2K2P2/8 b - g3 0 1",
-        #[rustfmt::skip]
         all: vec![
             "sbpf4f3-"
         ],
-        #[rustfmt::skip]
         attacks: vec![],
-        #[rustfmt::skip]
         attacks_checks: vec![],
+        promotes: vec![]
     })
 }
 
@@ -256,17 +242,17 @@ fn case_14() -> Result<()> {
 fn case_15() -> Result<()> {
     execute_test(TestCase {
         board: "1k6/b2P1r2/p5p1/4qp1p/7P/7K/8/8 w - - 0 1",
-        #[rustfmt::skip]
         all: vec![
             "pd7d8wn-", "pd7d8wb-", "pd7d8wr-", "pd7d8wq-",
             "swkh3g2-",
         ],
-        #[rustfmt::skip]
         attacks: vec![],
-        #[rustfmt::skip]
         attacks_checks: vec![
            "pd7d8wr-", "pd7d8wq-",
         ],
+        promotes: vec![
+            "pd7d8wn-", "pd7d8wb-", "pd7d8wr-", "pd7d8wq-",
+        ]
     })
 }
 
@@ -274,17 +260,17 @@ fn case_15() -> Result<()> {
 fn case_16() -> Result<()> {
     execute_test(TestCase {
         board: "8/bk1P1r2/p5p1/4qp1p/7P/7K/8/8 w - - 0 1",
-        #[rustfmt::skip]
         all: vec![
             "pd7d8wn-", "pd7d8wb-", "pd7d8wr-", "pd7d8wq-",
             "swkh3g2-",
         ],
-        #[rustfmt::skip]
         attacks: vec![],
-        #[rustfmt::skip]
         attacks_checks: vec![
             "pd7d8wn-",
         ],
+        promotes: vec![
+            "pd7d8wn-", "pd7d8wb-", "pd7d8wr-", "pd7d8wq-",
+        ]
     })
 }
 
@@ -292,18 +278,16 @@ fn case_16() -> Result<()> {
 fn case_17() -> Result<()> {
     execute_test(TestCase {
         board: "1r4rk/5pBp/ppbp1P2/5P2/2P3R1/3n3p/PP5P/5NK1 b - - 1 3",
-        #[rustfmt::skip]
         all: vec![
             "sbrg8g7wb"
         ],
-        #[rustfmt::skip]
         attacks: vec![
             "sbrg8g7wb"
         ],
-        #[rustfmt::skip]
         attacks_checks: vec![
             "sbrg8g7wb"
         ],
+        promotes: vec![],
     })
 }
 
@@ -311,7 +295,6 @@ fn case_17() -> Result<()> {
 fn case_18() -> Result<()> {
     execute_test(TestCase {
         board: "2r3k1/5p2/3R4/P4KpP/3PP3/5PP1/8/8 w - - 1 92",
-        #[rustfmt::skip]
         all: vec![
             "swph5h6-",
             "swpg3g4-",
@@ -322,15 +305,15 @@ fn case_18() -> Result<()> {
             "swpa5a6-",
             "swrd6d5-", "swrd6c6-", "swrd6b6-", "swrd6a6-", "swrd6d7-", "swrd6d8-", "swrd6e6-", "swrd6f6-", "swrd6g6-", "swrd6h6-",
         ],
-        #[rustfmt::skip]
         attacks: vec![
             "swkf5g5bp"
         ],
-        #[rustfmt::skip]
         attacks_checks: vec![
             "swkf5g5bp",
             "swrd6d8-", "swrd6g6-",
         ],
+        promotes: vec![
+        ]
     })
 }
 
@@ -338,7 +321,6 @@ fn case_18() -> Result<()> {
 fn case_19() -> Result<()> {
     execute_test(TestCase {
         board: "r4rk1/5ppp/8/8/8/8/5PPP/5RK1 b q - 6 27",
-        #[rustfmt::skip]
         all: vec![
             "sbph7h6-",
             "sbph7h5-",
@@ -351,12 +333,9 @@ fn case_19() -> Result<()> {
             "sbra8e8-", "sbra8d8-", "sbra8c8-", "sbra8c8-", "sbra8b8-",
             "sbra8a7-", "sbra8a6-", "sbra8a5-", "sbra8a4-", "sbra8a3-", "sbra8a2-", "sbra8a1-",
         ],
-        #[rustfmt::skip]
-        attacks: vec![
-        ],
-        #[rustfmt::skip]
-        attacks_checks: vec![
-        ],
+        attacks: vec![],
+        attacks_checks: vec![],
+        promotes: vec![]
     })
 }
 
@@ -364,18 +343,16 @@ fn case_19() -> Result<()> {
 fn case_20() -> Result<()> {
     execute_test(TestCase {
         board: "8/1p3B2/1n6/p3Pkp1/3P1pPp/1K3P1P/8/8 b - g3 0 41",
-        #[rustfmt::skip]
         all: vec![
             "ebf4g3g4", "ebh4g3g4"
         ],
-        #[rustfmt::skip]
         attacks: vec![
             "ebf4g3g4", "ebh4g3g4"
         ],
-        #[rustfmt::skip]
         attacks_checks: vec![
             "ebf4g3g4", "ebh4g3g4"
         ],
+        promotes: vec![]
     })
 }
 
@@ -396,6 +373,7 @@ fn case_21() {
         ],
         attacks: vec!["pg7f8wqbr", "pg7f8wrbr", "pg7f8wbbr", "pg7f8wnbr"],
         attacks_checks: vec!["pg7f8wqbr", "pg7f8wrbr", "pg7f8wbbr", "pg7f8wnbr"],
+        promotes: vec!["pg7f8wqbr", "pg7f8wrbr", "pg7f8wbbr", "pg7f8wnbr"]
     })
     .unwrap();
 }
@@ -404,11 +382,9 @@ fn case_21() {
 fn case_22() -> Result<()> {
     execute_test(TestCase {
         board: "8/1p3B2/1n6/p3Pkp1/3P1pP1/1K5P/8/4RR2 b - - 0 41",
-        #[rustfmt::skip]
         all: vec![],
-        #[rustfmt::skip]
         attacks: vec![],
-        #[rustfmt::skip]
         attacks_checks: vec![],
+        promotes: vec![]
     })
 }
